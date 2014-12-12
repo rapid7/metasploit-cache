4770131de8029e3ef511b303f67f9287641bb855
Luke Imhoff <luke_imhoff@rapid7.com>
2014-12-12 14:56:42 -0600

    metasploit-version install
    MSP-11132

--- metasploit-cache.gemspec
+++ metasploit-cache.gemspec
@@ -19,5 +19,6 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.7"
+  spec.add_development_dependency 'metasploit-version', '= 0.1.3.pre.changelog.pre.template'
   spec.add_development_dependency "rake", "~> 10.0"
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,76 @@
+require 'bundler/setup'
+
+#
+# Gems
+#
+
+require 'metasploit/version'
+
+#
+# Project
+#
+
+require 'metasploit/cache'
+
+# Use find_all_by_name instead of find_by_name as find_all_by_name will return pre-release versions
+gem_specification = Gem::Specification.find_all_by_name('metasploit-version').first
+
+Dir[File.join(gem_specification.gem_dir, 'spec', 'support', '**', '*.rb')].each do |f|
+  require f
+end
+
+RSpec.configure do |config|
+  config.expose_dsl_globally = false
+
+  # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  # allow more verbose output when running an individual spec file.
+  if config.files_to_run.one?
+    # RSpec filters the backtrace by default so as not to be so noisy.
+    # This causes the full backtrace to be printed when running a single
+    # spec file (e.g. to troubleshoot a particular spec failure).
+    config.full_backtrace = true
+  end
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    expectations.syntax = :expect
+  end
+
+  # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  config.mock_with :rspec do |mocks|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    # For more details, see:
+    #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+    mocks.syntax = :expect
+
+    mocks.patch_marshal_to_support_partial_doubles = false
+
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object.
+    mocks.verify_partial_doubles = true
+  end
+end
