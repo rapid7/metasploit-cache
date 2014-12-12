7551254052004ce6f899c96d44d79df9fc5a2efb
Luke Imhoff <luke_imhoff@rapid7.com>
2014-12-12 14:36:33 -0600

    bundle gem metasploit-cache
    MSP-11132

---
+++ lib/metasploit/cache.rb
@@ -0,0 +1,7 @@
+require "metasploit/cache/version"
+
+module Metasploit
+  module Cache
+    # Your code goes here...
+  end
+end
---
+++ lib/metasploit/cache/version.rb
@@ -0,0 +1,5 @@
+module Metasploit
+  module Cache
+    VERSION = "0.0.1"
+  end
+end
