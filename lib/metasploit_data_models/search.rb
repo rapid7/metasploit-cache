f80ca662dc892f9ff5f1fca49e20c9d65384f5ec
Luke Imhoff <luke_imhoff@rapid7.com>
2015-01-08 08:17:05 -0600

    Remove unused code under MetasploitDataModels
    MSP-11142

--- lib/metasploit_data_models/search.rb
+++
@@ -1,6 +0,0 @@
-module MetasploitDataModels
-  # Namespace that deals with search {Mdm} models.
-  module Search
-
-  end
-end
--- lib/metasploit_data_models/search/visitor.rb
+++
@@ -1,8 +0,0 @@
-module MetasploitDataModels
-  module Search
-    # Namespace for all visitors of `Metasploit::Model::Search::Query` that help search {Mdm} models.
-    module Visitor
-
-    end
-  end
-end
