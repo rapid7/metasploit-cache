7950e934882d31651e9bf3a2cc641a2a3ce9f3cc
Luke Imhoff <luke_imhoff@rapid7.com>
2015-01-08 15:25:28 -0600

    Merge Metasploit::Cache::Module::Author shared examples to spec
    MSP-11142
    
    Shared examples are no longer needed because Metasploit::Cache is the
    only implementor.

--- spec/app/models/metasploit/cache/module/author_spec.rb
+++ spec/app/models/metasploit/cache/module/author_spec.rb
@@ -1,7 +1,4 @@
 RSpec.describe Metasploit::Cache::Module::Author do
-  it_should_behave_like 'Metasploit::Cache::Module::Author',
-                        namespace_name: 'Metasploit::Cache'
-
   context 'associations' do
     it { should belong_to(:author).class_name('Metasploit::Cache::Author') }
     it { should belong_to(:email_address).class_name('Metasploit::Cache::EmailAddress') }
@@ -28,7 +25,31 @@ RSpec.describe Metasploit::Cache::Module::Author do
     end
   end
 
+  context 'factories' do
+    context :metasploit_cache_module_author do
+      subject(:metasploit_cache_module_author) do
+        FactoryGirl.build(:metasploit_cache_module_author)
+      end
+
+      it { should be_valid }
+    end
+
+    context :full_metasploit_cache_module_author do
+      subject(:full_metasploit_cache_module_author) do
+        FactoryGirl.build(:full_metasploit_cache_module_author)
+      end
+
+      it { should be_valid }
+
+      it 'has email_address' do
+        expect(full_metasploit_cache_module_author.email_address).not_to be_nil
+      end
+    end
+  end
+
   context 'validations' do
+    it { should validate_presence_of(:author) }
+
     context 'validates uniqueness of #author_id scoped to #module_instance_id' do
       let(:error) do
         I18n.translate!('metasploit.model.errors.messages.taken')
@@ -97,5 +118,8 @@ RSpec.describe Metasploit::Cache::Module::Author do
         end
       end
     end
+
+    it { should_not validate_presence_of(:email_address) }
+    it { should validate_presence_of(:module_instance) }
   end
 end
