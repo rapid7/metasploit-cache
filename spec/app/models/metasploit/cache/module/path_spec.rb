d0ffdb27f56a3c8c83b3a70b4f8ff28624d6a561
Luke Imhoff <luke_imhoff@rapid7.com>
2015-01-09 09:34:27 -0600

    Merge Metasploit::Cache::Module::Path shared examples into spec
    MSP-11142
    
    Shared examples are no longer needed because Metasploit::Cache is the
    only implementor.

--- spec/app/models/metasploit/cache/module/path_spec.rb
+++ spec/app/models/metasploit/cache/module/path_spec.rb
@@ -1,12 +1,100 @@
 RSpec.describe Metasploit::Cache::Module::Path do
-  it_should_behave_like 'Metasploit::Cache::Module::Path',
-                        namespace_name: 'Metasploit::Cache'
+  it { should be_a ActiveModel::Dirty }
+
+  it_should_behave_like 'Metasploit::Cache::RealPathname' do
+    let(:base_instance) do
+      FactoryGirl.build(:metasploit_cache_module_path)
+    end
+  end
 
   context 'associations' do
     it { should have_many(:module_ancestors).class_name('Metasploit::Cache::Module::Ancestor').dependent(:destroy).with_foreign_key(:parent_path_id) }
   end
 
   context 'callbacks' do
+    context 'before_validation' do
+      context 'nilify blanks' do
+        let(:path) do
+          FactoryGirl.build(
+              :metasploit_cache_module_path,
+              :gem => '',
+              :name => ''
+          )
+        end
+
+        it 'should have empty gem' do
+          expect(path.gem).not_to be_nil
+          expect(path.gem).to be_empty
+        end
+
+        it 'should have empty name' do
+          expect(path.name).not_to be_nil
+          expect(path.name).to be_empty
+        end
+
+        context 'after validation' do
+          before(:each) do
+            path.valid?
+          end
+
+          it 'does not have a gem' do
+            expect(path.gem).to be_nil
+          end
+
+          it 'does not have a name' do
+            expect(path.name).to be_nil
+          end
+        end
+      end
+
+      context '#normalize_real_path' do
+        let(:parent_pathname) do
+          Metasploit::Model::Spec.temporary_pathname.join('metasploit', 'cache', 'module', 'path')
+        end
+
+        let(:path) do
+          FactoryGirl.build(
+              :metasploit_cache_module_path,
+              :real_path => symlink_pathname.to_path
+          )
+        end
+
+        let(:real_basename) do
+          'real'
+        end
+
+        let(:real_pathname) do
+          parent_pathname.join(real_basename)
+        end
+
+        let(:symlink_basename) do
+          'symlink'
+        end
+
+        let(:symlink_pathname) do
+          parent_pathname.join(symlink_basename)
+        end
+
+        before(:each) do
+          real_pathname.mkpath
+
+          Dir.chdir(parent_pathname.to_path) do
+            File.symlink(real_basename, 'symlink')
+          end
+        end
+
+        it 'should convert real_path to a real path using File#real_path' do
+          expected_real_path = Metasploit::Model::File.realpath(path.real_path)
+
+          expect(path.real_path).not_to eq(expected_real_path)
+
+          path.valid?
+
+          expect(path.real_path).to eq(expected_real_path)
+        end
+      end
+    end
+
     context 'after update' do
       context '#update_module_ancestor_real_paths' do
         context 'with change to #real_path' do
@@ -66,7 +154,222 @@ RSpec.describe Metasploit::Cache::Module::Path do
     end
   end
 
+  context 'factories' do
+    context :metasploit_cache_module_path do
+      subject(:metasploit_cache_module_path) do
+        FactoryGirl.build(:metasploit_cache_module_path)
+      end
+
+      it { should be_valid }
+    end
+
+    context :named_metasploit_cache_module_path do
+      subject(:named_metasploit_cache_module_path) do
+        FactoryGirl.build(:named_metasploit_cache_module_path)
+      end
+
+      it { should be_valid }
+
+      it 'has a gem' do
+        expect(named_metasploit_cache_module_path.gem).not_to be_nil
+      end
+
+      it 'has a name' do
+        expect(named_metasploit_cache_module_path.name).not_to be_nil
+      end
+    end
+
+    context :unnamed_metasploit_cache_module_path do
+      subject(:unnamed_metasploit_cache_module_path) do
+        FactoryGirl.build(:unnamed_metasploit_cache_module_path)
+      end
+
+      it { should be_valid }
+
+      it 'does not have a gem' do
+        expect(unnamed_metasploit_cache_module_path.gem).to be_nil
+      end
+
+      it 'does not have a name' do
+        expect(unnamed_metasploit_cache_module_path.name).to be_nil
+      end
+    end
+  end
+
+  context 'mass assignment security' do
+    it { should allow_mass_assignment_of(:gem) }
+    it { should allow_mass_assignment_of(:name) }
+    it { should allow_mass_assignment_of(:real_path) }
+  end
+
   context 'validations' do
+    context 'directory' do
+      let(:error) do
+        'must be a directory'
+      end
+
+      let(:path) do
+        FactoryGirl.build(
+            :metasploit_cache_module_path,
+            :real_path => real_path
+        )
+      end
+
+      before(:each) do
+        path.valid?
+      end
+
+      context 'with #real_path' do
+        context 'with directory' do
+          let(:real_path) do
+            FactoryGirl.generate :metasploit_cache_module_path_directory_real_path
+          end
+
+          it 'should not record error on real_path' do
+            path.valid?
+
+            expect(path.errors[:real_path]).not_to include(error)
+          end
+        end
+
+        context 'with file' do
+          let(:pathname) do
+            Metasploit::Model::Spec.temporary_pathname.join(
+                'metasploit',
+                'cache',
+                'module',
+                'path',
+                'real',
+                'path',
+                'file'
+            )
+          end
+
+          let(:real_path) do
+            pathname.to_path
+          end
+
+          before(:each) do
+            Metasploit::Model::Spec::PathnameCollision.check!(pathname)
+
+            pathname.parent.mkpath
+
+            pathname.open('wb') do |f|
+              f.puts 'A file'
+            end
+          end
+
+          it 'should record error on real_path' do
+            path.valid?
+
+            expect(path.errors[:real_path]).to include(error)
+          end
+        end
+      end
+
+      context 'without #real_path' do
+        let(:real_path) do
+          nil
+        end
+
+        it 'should record error on real_path' do
+          path.valid?
+
+          expect(path.errors[:real_path]).to include(error)
+        end
+      end
+    end
+
+    context 'gem and name' do
+      let(:gem_error) do
+        "can't be blank if name is present"
+      end
+
+      let(:name_error) do
+        "can't be blank if gem is present"
+      end
+
+      subject(:path) do
+        FactoryGirl.build(
+            :metasploit_cache_module_path,
+            :gem => gem,
+            :name => name
+        )
+      end
+
+      before(:each) do
+        path.valid?
+      end
+
+      context 'with gem' do
+        let(:gem) do
+          FactoryGirl.generate :metasploit_cache_module_path_gem
+        end
+
+        context 'with name' do
+          let(:name) do
+            FactoryGirl.generate :metasploit_cache_module_path_name
+          end
+
+          it 'should not record error on gem' do
+            expect(path.errors[:gem]).not_to include(gem_error)
+          end
+
+          it 'should not record error on name' do
+            expect(path.errors[:name]).not_to include(name_error)
+          end
+        end
+
+        context 'without name' do
+          let(:name) do
+            nil
+          end
+
+          it 'should not record error on gem' do
+            expect(path.errors[:gem]).not_to include(gem_error)
+          end
+
+          it 'should record error on name' do
+            expect(path.errors[:name]).to include(name_error)
+          end
+        end
+      end
+
+      context 'without gem' do
+        let(:gem) do
+          nil
+        end
+
+        context 'with name' do
+          let(:name) do
+            FactoryGirl.generate :metasploit_cache_module_path_name
+          end
+
+          it 'should record error on gem' do
+            expect(path.errors[:gem]).to include(gem_error)
+          end
+
+          it 'should not record error on name' do
+            expect(path.errors[:name]).not_to include(name_error)
+          end
+        end
+
+        context 'without name' do
+          let(:name) do
+            nil
+          end
+
+          it 'should not record error on gem' do
+            expect(path.errors[:gem]).not_to include(gem_error)
+          end
+
+          it 'should not record error on name' do
+            expect(path.errors[:name]).not_to include(name_error)
+          end
+        end
+      end
+    end
+
     context 'validate unique of name scoped to gem' do
       context 'with different real_paths' do
         let(:duplicate) do
@@ -103,6 +406,68 @@ RSpec.describe Metasploit::Cache::Module::Path do
         expect(duplicate).not_to be_valid
         expect(duplicate.errors[:real_path]).to include('has already been taken')
       end
+    end
+  end
+
+  context '#directory?' do
+    subject(:directory?) do
+      path.directory?
+    end
+
+    let(:path) do
+      FactoryGirl.build(
+          :metasploit_cache_module_path,
+          :real_path => real_path
+      )
+    end
+
+    context 'with #real_path' do
+      context 'with file' do
+        let(:pathname) do
+          Metasploit::Model::Spec.temporary_pathname.join(
+              'metasploit',
+              'cache',
+              'module',
+              'path',
+              'real',
+              'path',
+              'file'
+          )
+        end
+
+        let(:real_path) do
+          pathname.to_path
+        end
+
+        before(:each) do
+          Metasploit::Model::Spec::PathnameCollision.check!(pathname)
+
+          pathname.parent.mkpath
+
+          pathname.open('wb') do |f|
+            f.puts 'A file'
+          end
+        end
+
+
+        it { is_expected.to eq(false) }
+      end
+
+      context 'with directory' do
+        let(:real_path) do
+          FactoryGirl.generate :metasploit_cache_module_path_directory_real_path
+        end
+
+        it { is_expected.to eq(true) }
+      end
+    end
+
+    context 'without #real_path' do
+      let(:real_path) do
+        nil
+      end
+
+      it { is_expected.to eq(false) }
     end
   end
 
@@ -681,6 +1046,64 @@ RSpec.describe Metasploit::Cache::Module::Path do
     end
   end
 
+  context '#named?' do
+    subject(:named?) do
+      path.named?
+    end
+
+    let(:path) do
+      FactoryGirl.build(
+          :metasploit_cache_module_path,
+          :gem => gem,
+          :name => name
+      )
+    end
+
+    context 'with blank gem' do
+      let(:gem) do
+        ''
+      end
+
+      context 'with blank name' do
+        let(:name) do
+          ''
+        end
+
+        it { is_expected.to eq(false) }
+      end
+
+      context 'without blank name' do
+        let(:name) do
+          FactoryGirl.generate :metasploit_cache_module_path_name
+        end
+
+        it { is_expected.to eq(false) }
+      end
+    end
+
+    context 'without blank gem' do
+      let(:gem) do
+        FactoryGirl.generate :metasploit_cache_module_path_gem
+      end
+
+      context 'with blank name' do
+        let(:name) do
+          ''
+        end
+
+        it { is_expected.to eq(false) }
+      end
+
+      context 'without blank name' do
+        let(:name) do
+          FactoryGirl.generate :metasploit_cache_module_path_name
+        end
+
+        it { is_expected.to eq(true) }
+      end
+    end
+  end
+
   context '#name_collision' do
     subject(:name_collision) do
       path.name_collision
@@ -760,6 +1183,80 @@ RSpec.describe Metasploit::Cache::Module::Path do
       end
 
       it { should be_nil }
+    end
+  end
+
+  context '#was_named?' do
+    subject(:was_named?) do
+      path.was_named?
+    end
+
+    let(:gem) do
+      FactoryGirl.generate :metasploit_cache_module_path_gem
+    end
+
+    let(:name) do
+      FactoryGirl.generate :metasploit_cache_module_path_name
+    end
+
+    let(:path) do
+      FactoryGirl.build(
+          :metasploit_cache_module_path
+      )
+    end
+
+    before(:each) do
+      path.gem = gem_was
+      path.name = name_was
+
+      path.changed_attributes.clear
+
+      path.gem = gem
+      path.name = name
+    end
+
+    context 'with blank gem_was' do
+      let(:gem_was) do
+        nil
+      end
+
+      context 'with blank name_was' do
+        let(:name_was) do
+          nil
+        end
+
+        it { is_expected.to eq(false) }
+      end
+
+      context 'without blank name_was' do
+        let(:name_was) do
+          FactoryGirl.generate :metasploit_cache_module_path_name
+        end
+
+        it { is_expected.to eq(false) }
+      end
+    end
+
+    context 'without blank gem_was' do
+      let(:gem_was) do
+        FactoryGirl.generate :metasploit_cache_module_path_gem
+      end
+
+      context 'with blank name_was' do
+        let(:name_was) do
+          nil
+        end
+
+        it { is_expected.to eq(false) }
+      end
+
+      context 'without blank name_was' do
+        let(:name_was) do
+          FactoryGirl.generate :metasploit_cache_module_path_name
+        end
+
+        it { is_expected.to eq(true) }
+      end
     end
   end
 end
