13e74f79751398011d6c394c019f4e964936dee2
Luke Imhoff <luke_imhoff@rapid7.com>
2015-01-09 10:28:43 -0600

    Merge Metasploit::Cache::Module::Target::Architecture shared examples to spec
    MSP-11142
    
    Shared examples are no longer needed because Metasploit::Cache is the
    only implementor.

--- spec/app/models/metasploit/cache/module/target/architecture_spec.rb
+++ spec/app/models/metasploit/cache/module/target/architecture_spec.rb
@@ -1,7 +1,4 @@
 RSpec.describe Metasploit::Cache::Module::Target::Architecture do
-  it_should_behave_like 'Metasploit::Cache::Module::Target::Architecture',
-                        namespace_name: 'Metasploit::Cache'
-
   context 'associations' do
     it { should belong_to(:architecture).class_name('Metasploit::Cache::Architecture') }
     it { should belong_to(:module_target).class_name('Metasploit::Cache::Module::Target') }
@@ -18,7 +15,64 @@ RSpec.describe Metasploit::Cache::Module::Target::Architecture do
     end
   end
 
+  context 'factories' do
+    context :metasploit_cache_module_target_architecture do
+      subject(:metasploit_cache_module_target_architecture) do
+        FactoryGirl.build(:metasploit_cache_module_target_architecture)
+      end
+
+      it { should be_valid }
+
+      context '#module_target' do
+        subject(:module_target) do
+          metasploit_cache_module_target_architecture.module_target
+        end
+
+        it { should be_valid }
+
+        context '#module_instance' do
+          subject(:module_instance) do
+            module_target.module_instance
+          end
+
+          it { should be_valid }
+
+          context '#targets' do
+            subject(:targets) do
+              module_instance.targets
+            end
+
+            it 'has one' do
+              expect(targets.length).to eq(1)
+            end
+
+            it 'should include #module_target' do
+              expect(targets).to include module_target
+            end
+          end
+        end
+
+        context '#target_architectures' do
+          subject(:target_architectures) do
+            module_target.target_architectures
+          end
+
+          it 'has one' do
+            expect(target_architectures.length).to eq(1)
+          end
+
+          it "should include #{:metasploit_cache_module_target_architecture}" do
+            expect(target_architectures).to include metasploit_cache_module_target_architecture
+          end
+        end
+      end
+    end
+  end
+
   context 'validations' do
+    it { should validate_presence_of :architecture }
+    it { should validate_presence_of :module_target }
+
     context 'validates uniqueness of architecture_id scoped to module_target_id' do
       #
       # lets
