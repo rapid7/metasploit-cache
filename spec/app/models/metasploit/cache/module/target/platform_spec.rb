478d1624345d9bba61876b4703db568aaa01c5f9
Luke Imhoff <luke_imhoff@rapid7.com>
2015-01-09 10:46:25 -0600

    Merge Metasploit::Cache::Module::Target::Platform shared examples to spec
    MSP-11142
    
    Shared examples are no longer needed because Metasploit::Cache is the
    only implementor.

--- spec/app/models/metasploit/cache/module/target/platform_spec.rb
+++ spec/app/models/metasploit/cache/module/target/platform_spec.rb
@@ -1,7 +1,4 @@
 RSpec.describe Metasploit::Cache::Module::Target::Platform do
-  it_should_behave_like 'Metasploit::Cache::Module::Target::Platform',
-                        namespace_name: 'Metasploit::Cache'
-
   context 'associations' do
     it { should belong_to(:module_target).class_name('Metasploit::Cache::Module::Target') }
     it { should belong_to(:platform).class_name('Metasploit::Cache::Platform') }
@@ -18,7 +15,64 @@ RSpec.describe Metasploit::Cache::Module::Target::Platform do
     end
   end
 
+  context 'factories' do
+    context module_target_platform_factory do
+      subject(module_target_platform_factory) do
+        FactoryGirl.build(module_target_platform_factory)
+      end
+
+      it { should be_valid }
+
+      context '#module_target' do
+        subject(:module_target) do
+          send(module_target_platform_factory).module_target
+        end
+
+        it { should be_valid }
+
+        context '#module_instance' do
+          subject(:module_instance) do
+            module_target.module_instance
+          end
+
+          it { should be_valid }
+
+          context '#targets' do
+            subject(:targets) do
+              module_instance.targets
+            end
+
+            it 'has one' do
+              expect(targets.length).to eq(1)
+            end
+
+            it 'should include #module_target' do
+              expect(targets).to include module_target
+            end
+          end
+        end
+
+        context '#target_platforms' do
+          subject(:target_platforms) do
+            module_target.target_platforms
+          end
+
+          it 'has one' do
+            expect(target_platforms.length).to eq(1)
+          end
+
+          it "should include #{module_target_platform_factory}" do
+            expect(target_platforms).to include send(module_target_platform_factory)
+          end
+        end
+      end
+    end
+  end
+
   context 'validations' do
+    it { should validate_presence_of :module_target }
+    it { should validate_presence_of :platform }
+
     context 'validates uniqueness of platform_id scoped to module_target_id' do
       #
       # lets
