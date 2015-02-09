65111845a65c3e85903ba5a83694fa3ca654bf46
Trevor Rosen <trevor_rosen@rapid7.com>
2015-02-09 15:16:20 -0600

    Remove pre-release constant

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -12,8 +12,6 @@ module Metasploit
       MINOR = 28
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 0
-      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'extract-cache-from-metasploit-model'
 
       #
       # Module Methods
