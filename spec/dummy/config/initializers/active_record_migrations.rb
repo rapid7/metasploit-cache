d36f78a0cb088b459a30f033aef095b981f41ac8
Luke Imhoff <luke_imhoff@rapid7.com>
2015-02-18 14:29:48 -0600

    Remove active_record_migrations initializer from dummy app.
    MSP-12083
    
    The engine's db/migrate is automatically added and the initializer as
    written reset the migrations_paths to just metasploit-cache's
    db/migrate, which remove metasploit_data_models' and
    metasploit-credential's when running `rake db:create db:migrate`.

--- spec/dummy/config/initializers/active_record_migrations.rb
+++
@@ -1,4 +0,0 @@
-# remove Dummy's db/migrate, which doesn't exists and replace it with gem's
-ActiveRecord::Migrator.migrations_paths = [
-    Metasploit::Cache::Engine.root.join('db', 'migrate').to_path
-]
