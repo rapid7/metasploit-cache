6cface9225a8389abeb1742ffc2df79fb44537d8
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-06 13:32:16 -0600

    Update version for branch
    MSP-12271

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -11,9 +11,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 63
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 0
+      PATCH = 1
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'metasploit-cache-module-ancestor-sti'
+      PRERELEASE = 'direct-class'
 
       #
       # Module Methods
