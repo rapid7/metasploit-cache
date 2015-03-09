c1380dfee6e615f836e0fa67d3002aa7d9e07d00
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-09 16:06:50 -0500

    Metasploit::Cache::Direct::Class::Cache
    MSP-12278
    
    Ephemeral cache for `Metasploit::Cache::Direct::Class`, similar to
    `Metasploit::Cache::Module::Ancestor::Cache` is the ephemeral cache for
    `Metasploit::Cache::Module::Ancestor`.

---
+++ app/models/metasploit/cache/direct/class/cache.rb
@@ -0,0 +1,73 @@
+# Ephemeral Cache for connecting an in-memory Metasploit Module's ruby Class to its persisted
+# {Metasploit::Cache::Module::Class} and {Metasploit::Cache::Module::Ancestor}.
+class Metasploit::Cache::Direct::Class::Cache < Metasploit::Model::Base
+  extend Metasploit::Cache::ResurrectingAttribute
+
+  #
+  # Attributes
+  #
+
+  # The ephemeral cache for just the {Metasploit::Cache::Module::Ancestor} for this Ruby Class.
+  #
+  # @return [Metasploit::Cache::Module::Ancestor::Cache]
+  attr_accessor :module_ancestor_cache
+
+  # The subclass of {Metasploit::Cache::Direct::Class} to use to look up {#direct_class}.
+  #
+  # @return [Class<Metasploit::Cache::Direct::Class>]
+  attr_accessor :direct_class_class
+
+  #
+  # Resurrecting Attributes
+  #
+
+  # Cached metadata for this Class.
+  #
+  # @return [Metasploit::Cache::Direct::Class]
+  resurrecting_attr_accessor :direct_class do
+    ActiveRecord::Base.connection_pool.with_connection {
+      direct_class_class.where(
+          Metasploit::Cache::Module::Ancestor.arel_table[:real_path_sha1_hex_digest].eq(real_path_sha1_hex_digest)
+      ).joins(:ancestor).readonly(false).first
+    }
+  end
+
+  #
+  # Validations
+  #
+
+  validates :direct_class_class,
+            presence: true
+  validates :module_ancestor_cache,
+            presence: true
+
+  #
+  # Instance Methods
+  #
+
+  delegate :logger,
+           :metasploit_module,
+           :module_ancestor,
+           :real_path_sha1_hex_digest,
+           to: :module_ancestor_cache
+
+  # @note Validation errors for `direct_class` will be logged as errors tagged with
+  # {Metasploit::Cache::Module::Ancestor#real_pathname}/.
+  #
+  # @param to [Metasploit::Cache::Direct::Class] Save cacheable data to {Metasploit::Cache::Direct::Class}.
+  # @return [Metasploit::Cache::Direct::Class] `#persisted?` will be `false` if saving fails.
+  def persist_direct_class(to: direct_class)
+    # Ensure that connection is only held temporarily by Thread instead of being memoized to Thread
+    ActiveRecord::Base.connection_pool.with_connection do
+      unless to.batched_save
+        logger.tagged(module_ancestor.real_pathname.to_s) do |tagged|
+          tagged.error {
+            "Could not be persisted to #{to.class}: #{to.errors.full_messages.to_sentence}"
+          }
+        end
+      end
+    end
+
+    to
+  end
+end
---
+++ spec/app/models/metasploit/cache/direct/class/cache_spec.rb
@@ -0,0 +1,179 @@
+RSpec.describe Metasploit::Cache::Direct::Class::Cache do
+  subject(:direct_class_cache) {
+    described_class.new(
+        direct_class_class: expected_direct_class.class,
+        module_ancestor_cache: module_ancestor_cache
+    )
+  }
+
+  let(:expected_direct_class) {
+    FactoryGirl.build(expected_direct_class_factory)
+  }
+
+  let(:expected_direct_class_factory) {
+    FactoryGirl.generate :metasploit_cache_direct_class_factory
+  }
+
+  let(:metasploit_module) {
+    Module.new
+  }
+
+  let(:module_ancestor) {
+    expected_direct_class.ancestor
+  }
+
+  let(:module_ancestor_cache) {
+    Metasploit::Cache::Module::Ancestor::Cache.new(
+        metasploit_module: metasploit_module,
+        real_path_sha1_hex_digest: real_path_sha1_hex_digest
+    )
+  }
+
+  let(:real_path_sha1_hex_digest) {
+    module_ancestor.real_path_sha1_hex_digest
+  }
+
+  context 'resurrecting attributes' do
+    context '#direct_class' do
+      subject(:direct_class) {
+        direct_class_cache.direct_class
+      }
+
+      before(:each) do
+        expected_direct_class.save!
+
+        # have to stub because real_path_sha1_hex_digest is normally delegated to the namespace parent
+        allow(module_ancestor_cache).to receive(:real_path_sha1_hex_digest).and_return(module_ancestor.real_path_sha1_hex_digest)
+      end
+
+      it 'is an instance of a subclass of Metasploit::Cache::Direct::Class' do
+        expect(direct_class.class).to be < Metasploit::Cache::Direct::Class
+      end
+
+      it 'is a Metasploit::Cache::Direct::Class with #ancestor matching pre-existing Metasploit::Cache::Module::Ancestor' do
+        expect(direct_class).to eq(expected_direct_class)
+        expect(direct_class.ancestor).to eq(module_ancestor)
+      end
+    end
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:direct_class_class) }
+    it { is_expected.to validate_presence_of(:module_ancestor_cache) }
+  end
+
+  context '#persist_direct_class' do
+    subject(:persist_direct_class) do
+      direct_class_cache.persist_direct_class(*args)
+    end
+
+    context 'with :to' do
+      let(:args) {
+        [
+            {
+                to: expected_direct_class
+            }
+        ]
+      }
+
+      it 'does not access default #direct_class' do
+        expect(direct_class_cache).not_to receive(:direct_class)
+
+        persist_direct_class
+      end
+
+      it 'uses :to' do
+        expect(expected_direct_class).to receive(:batched_save).and_call_original
+
+        persist_direct_class
+      end
+
+      context 'batched save' do
+        context 'failure' do
+          #
+          # lets
+          #
+
+          let(:logger) {
+            ActiveSupport::TaggedLogging.new(
+                Logger.new(string_io)
+            )
+          }
+
+          let(:string_io) {
+            StringIO.new
+          }
+
+          let(:success) {
+            false
+          }
+
+          #
+          # Callbacks
+          #
+
+          before(:each) do
+            expected_direct_class.rank = nil
+            expected_direct_class.valid?
+            module_ancestor_cache.logger = logger
+          end
+
+          it 'tags log with Metasploit::Cache::Module::Ancestor#real_path' do
+            persist_direct_class
+
+            expect(string_io.string).to include("[#{module_ancestor.real_pathname.to_s}]")
+          end
+
+
+          it 'logs validation errors' do
+            persist_direct_class
+
+            full_error_messages = expected_direct_class.errors.full_messages.to_sentence
+
+            expect(full_error_messages).not_to be_blank
+            expect(string_io.string).to include("Could not be persisted to #{expected_direct_class.class}: #{full_error_messages}")
+          end
+        end
+
+
+        context 'success' do
+          specify {
+            expect {
+              persist_direct_class
+            }.to change(Metasploit::Cache::Direct::Class, :count).by(1)
+          }
+        end
+      end
+    end
+
+    context 'without :to' do
+      #
+      # lets
+      #
+
+      let(:args) {
+        []
+      }
+
+      #
+      # Callbacks
+      #
+
+      before(:each) do
+        expected_direct_class.save!
+      end
+
+      it 'defaults to #direct_class' do
+        expect(direct_class_cache).to receive(:direct_class).and_call_original
+
+        persist_direct_class
+      end
+
+      it 'uses #module_ancestor' do
+        expect(direct_class_cache.direct_class).to receive(:batched_save).and_call_original
+
+        persist_direct_class
+      end
+    end
+  end
+end
