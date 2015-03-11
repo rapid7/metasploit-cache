bfd1e662bd0df6f57fb0124991120343b9a85e6e
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-11 16:37:25 -0500

    Test Payload::Ancestor factory loading
    MSP-12278

--- spec/support/shared/examples/metasploit/cache/payload/ancestor/factory.rb
+++ spec/support/shared/examples/metasploit/cache/payload/ancestor/factory.rb
@@ -1,12 +1,12 @@
 RSpec.shared_examples_for 'Metasploit::Cache::Payload::Ancestor factory' do |payload_type:|
+  let(:module_ancestor) {
+    payload_ancestor
+  }
+
   it { is_expected.to be_valid }
 
   context 'contents' do
     include_context 'Metasploit::Cache::Module::Ancestor factory contents'
-
-    let(:module_ancestor) {
-      payload_ancestor
-    }
 
     context 'metasploit_module' do
       include_context 'Metasploit::Cache::Module::Ancestor factory contents metasploit_module'
@@ -20,6 +20,38 @@ RSpec.shared_examples_for 'Metasploit::Cache::Payload::Ancestor factory' do |pay
         expect(unbound_method.parameters.length).to eq(1)
         expect(unbound_method.parameters[0][0]).to eq(:opt)
       end
+    end
+  end
+
+  context 'loading' do
+    include_context 'Metasploit::Cache::Module::Ancestor::Spec::Unload.unload'
+
+    context 'Metasploit::Cache::Module::Ancestor::Load' do
+      subject(:module_ancestor_load) {
+        Metasploit::Cache::Module::Ancestor::Load.new(
+            logger: logger,
+            maximum_version: 4,
+            module_ancestor: module_ancestor
+        )
+      }
+
+      let(:logger) {
+        ActiveSupport::TaggedLogging.new(
+            Logger.new(string_io)
+        )
+      }
+
+      let(:string_io) {
+        StringIO.new
+      }
+
+      it { is_expected.to be_valid }
+
+      specify {
+        expect {
+          module_ancestor_load.valid?
+        }.to change(described_class, :count).from(0).to(1)
+      }
     end
   end
 
