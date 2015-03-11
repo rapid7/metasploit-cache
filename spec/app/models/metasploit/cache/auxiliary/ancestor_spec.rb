0f19f3344c065acdb6a8cfaae7781dada3167b21
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-11 16:18:58 -0500

    'Metasploit::Cache::Module::Ancestor factory' shared example
    MSP-12278
    
    Move duplicate code to shared example.

--- spec/app/models/metasploit/cache/auxiliary/ancestor_spec.rb
+++ spec/app/models/metasploit/cache/auxiliary/ancestor_spec.rb
@@ -13,42 +13,10 @@ RSpec.describe Metasploit::Cache::Auxiliary::Ancestor do
         FactoryGirl.build(:metasploit_cache_auxiliary_ancestor)
       }
 
-      it { is_expected.to be_valid }
-
-      context 'contents' do
-        include_context 'Metasploit::Cache::Module::Ancestor factory contents'
-
-        let(:module_ancestor) do
+      it_should_behave_like 'Metasploit::Cache::Module::Ancestor factory' do
+        let(:module_ancestor) {
           metasploit_cache_auxiliary_ancestor
-        end
-
-        context 'metasploit_module' do
-          include_context 'Metasploit::Cache::Module::Ancestor factory contents metasploit_module'
-
-          it { should be_a Class }
-
-          context '#initialize' do
-            subject(:instance) do
-              metasploit_module.new(attributes)
-            end
-
-            context 'with :framework' do
-              let(:attributes) do
-                {
-                    framework: framework
-                }
-              end
-
-              let(:framework) do
-                double('Msf::Framework')
-              end
-
-              it 'should set #framework' do
-                expect(instance.framework).to eq(framework)
-              end
-            end
-          end
-        end
+        }
       end
     end
   end
---
+++ spec/support/shared/examples/metasploit/cache/module/ancestor/factory.rb
@@ -0,0 +1,35 @@
+RSpec.shared_examples_for 'Metasploit::Cache::Module::Ancestor factory' do
+  it { is_expected.to be_valid }
+
+  context 'contents' do
+    include_context 'Metasploit::Cache::Module::Ancestor factory contents'
+
+    context 'metasploit_module' do
+      include_context 'Metasploit::Cache::Module::Ancestor factory contents metasploit_module'
+
+      it { should be_a Class }
+
+      context '#initialize' do
+        subject(:instance) do
+          metasploit_module.new(attributes)
+        end
+
+        context 'with :framework' do
+          let(:attributes) do
+            {
+                framework: framework
+            }
+          end
+
+          let(:framework) do
+            double('Msf::Framework')
+          end
+
+          it 'should set #framework' do
+            expect(instance.framework).to eq(framework)
+          end
+        end
+      end
+    end
+  end
+end
