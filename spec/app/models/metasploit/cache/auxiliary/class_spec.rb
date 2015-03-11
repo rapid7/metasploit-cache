dbecdf900ad949d1a0e470c9dc8cf1cb44d5e09b
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-11 15:42:57 -0500

    Test that metasploit_cache_auxiliary_class factory is loadable
    MSP-12278
    
    Ensures that templates are compatible with Loads.

--- spec/app/models/metasploit/cache/auxiliary/class_spec.rb
+++ spec/app/models/metasploit/cache/auxiliary/class_spec.rb
@@ -13,6 +13,63 @@ RSpec.describe Metasploit::Cache::Auxiliary::Class do
       }
 
       it { is_expected.to be_valid }
+
+      context 'loading' do
+        include_context 'Metasploit::Cache::Module::Ancestor::Spec::Unload.unload'
+
+        let(:logger) {
+          ActiveSupport::TaggedLogging.new(
+              Logger.new(string_io)
+          )
+        }
+
+        let(:module_ancestor_load) {
+          Metasploit::Cache::Module::Ancestor::Load.new(
+              logger: logger,
+              maximum_version: 4,
+              module_ancestor: metasploit_cache_auxiliary_class.ancestor
+          )
+        }
+
+        let(:string_io) {
+          StringIO.new
+        }
+
+        before(:each) do
+          # To prove Direct::Class::Load is set rank
+          metasploit_cache_auxiliary_class.rank = nil
+        end
+
+        context 'Metasploit::Cache::Module::Ancestor::Load' do
+          subject {
+            module_ancestor_load
+          }
+
+          it { is_expected.to be_valid }
+        end
+
+        context 'Metasploit::Cache::Direct::Class::Load' do
+          subject(:direct_class_load) {
+            Metasploit::Cache::Direct::Class::Load.new(
+                direct_class: metasploit_cache_auxiliary_class,
+                logger: logger,
+                metasploit_module: module_ancestor_load.metasploit_module
+            )
+          }
+
+          before(:each) do
+            expect(module_ancestor_load).to be_valid
+          end
+
+          it { is_expected.to be_valid }
+
+          specify {
+            expect {
+              direct_class_load.valid?
+            }.to change(described_class, :count).from(0).to(1)
+          }
+        end
+      end
     end
   end
 end
