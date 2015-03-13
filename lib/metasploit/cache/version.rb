e036211b19f8170e2a0dfb2808e5b329f0e5d7df
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-13 09:26:33 -0500

    Update version for branch
    MSP-12297

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -13,7 +13,7 @@ module Metasploit
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 4
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'post-class'
+      PRERELEASE = 'load-post-class-from-framework'
 
       #
       # Module Methods
