d3ef6d2e5e7e231472c45928a4cb6772a2ee8133
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-16 11:21:46 -0500

    :metasploit_cache_direct_class_ancestor_content
    MSP-12300
    
    Separate writing template from `:metasploit_cache_direct_class` trait as
    was done with separating `:metasploit_cache_module_ancestor` and
    `:metasploit_cache_module_ancestor_content`.

--- spec/factories/metasploit/cache/auxiliary/classes.rb
+++ spec/factories/metasploit/cache/auxiliary/classes.rb
@@ -1,7 +1,10 @@
 FactoryGirl.define do
   factory :metasploit_cache_auxiliary_class,
           class: Metasploit::Cache::Auxiliary::Class,
-          traits: [:metasploit_cache_direct_class] do
+          traits: [
+              :metasploit_cache_direct_class,
+              :metasploit_cache_direct_class_ancestor_content
+          ] do
     #
     # Associations
     #
