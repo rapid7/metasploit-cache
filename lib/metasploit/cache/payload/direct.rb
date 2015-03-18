c8869d17a87ca58060448aa19a308afc68780a91
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-18 12:50:14 -0500

    Fix yard warning
    MSP-12298

--- lib/metasploit/cache/payload/direct.rb
+++ lib/metasploit/cache/payload/direct.rb
@@ -1,5 +1,5 @@
 # Namespace for {Metasploit::Cache::Payload::Direct::Class class metadata} that is generated from one payload
-# {Metasploit::Cache::Payload::Direct::Class#ancestor ancestor's Metasploit Module}.
+# {Metasploit::Cache::Direct::Class#ancestor ancestor's Metasploit Module}.
 module Metasploit::Cache::Payload::Direct
   extend ActiveSupport::Autoload
 
