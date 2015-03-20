59afa42d27cdf776616b92edd7d04ee48c54c782
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-20 08:30:01 -0500

    A link from namespace to class for Payload::Stager
    MSP-12345

--- lib/metasploit/cache/payload/stager.rb
+++ lib/metasploit/cache/payload/stager.rb
@@ -1,5 +1,5 @@
 # Namespace for stager payload Metasploit Module cache metadata, including
-# {Metasploit::Cache::Payload::Stager::Ancestor ancestors}.
+# {Metasploit::Cache::Payload::Stager::Ancestor ancestors} and {Metasploit::Cache::Payload::Stager::Class classes}.
 module Metasploit::Cache::Payload::Stager
   extend ActiveSupport::Autoload
 
