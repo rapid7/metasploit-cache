3506c3895572c6ebbb50b65e9e68965fe1ac8529
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-20 08:19:37 -0500

    Red specs for Payload::Stager::Class
    MSP-12345

---
+++ app/models/metasploit/cache/payload/stager/class.rb
@@ -0,0 +1,3 @@
+# Class-level metadata for a stager payload Metasploit Module.
+class Metasploit::Cache::Payload::Stager::Class < Metasploit::Cache::Payload::Direct::Class
+end
--- lib/metasploit/cache/payload/stager.rb
+++ lib/metasploit/cache/payload/stager.rb
@@ -4,4 +4,5 @@ module Metasploit::Cache::Payload::Stager
   extend ActiveSupport::Autoload
 
   autoload :Ancestor
+  autoload :Class
 end
