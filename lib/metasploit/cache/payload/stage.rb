df8ec8506bfb02e0fe684728e0047fe68816441d
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-20 07:04:06 -0500

    Add link from namespace to Payload::Stage::Class
    MSP-12343

--- lib/metasploit/cache/payload/stage.rb
+++ lib/metasploit/cache/payload/stage.rb
@@ -1,5 +1,5 @@
 # Namespace for stage payload Metasploit Module cache metadata, including
-# {Metasploit::Cache::Payload::Stage::Ancestor ancestors}.
+# {Metasploit::Cache::Payload::Stage::Ancestor ancestors} and {Metasploit::Cache::Payload::Stage::Class classes}.
 module Metasploit::Cache::Payload::Stage
   extend ActiveSupport::Autoload
 
