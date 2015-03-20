b0fb1de211793b8d092cf12feeef0f675c0ad7bb
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-20 08:28:54 -0500

    Green payload/stager/class_spec.rb
    MSP-12345

--- app/models/metasploit/cache/payload/stager/class.rb
+++ app/models/metasploit/cache/payload/stager/class.rb
@@ -4,8 +4,15 @@ class Metasploit::Cache::Payload::Stager::Class < Metasploit::Cache::Payload::Di
   # Associations
   #
 
+  # Metadata for file that defined the ruby Module.
+  belongs_to :ancestor,
+             class_name: 'Metasploit::Cache::Payload::Stager::Ancestor',
+             inverse_of: :stager_payload_class
+
   # Reliability of Metasploit Module.
   belongs_to :rank,
              class_name: 'Metasploit::Cache::Module::Rank',
              inverse_of: :stager_payload_classes
+
+  Metasploit::Concern.run(self)
 end
