b3c3559b8993ca6c08d6b55f0a28f8f655b857fa
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-23 12:41:08 -0500

    Red Metasploit::Cache::Auxiliary::Instance#actions validation spec
    MSP-12352

--- spec/app/models/metasploit/cache/auxiliary/instance_spec.rb
+++ spec/app/models/metasploit/cache/auxiliary/instance_spec.rb
@@ -26,5 +26,47 @@ RSpec.describe Metasploit::Cache::Auxiliary::Instance, type: :model do
 
   context 'validations' do
     it { is_expected.to validate_presence_of(:auxiliary_class) }
+
+    # validate_lenght_of from shoulda-matchers assumes attribute is String and doesn't work on associations
+    context 'validates length of actions is at least 1' do
+      let(:error) {
+        I18n.translate!('activerecord.errors.models.metasploit/cache/auxiliary/instance.attributes.actions.too_short')
+      }
+
+      context 'without actions' do
+        subject(:auxiliary_instance) {
+          FactoryGirl.build(
+              :metasploit_cache_auxiliary_instance,
+              actions: []
+          )
+        }
+
+        it 'adds error on #actions' do
+          auxiliary_instance.valid?
+
+          expect(auxiliary_instance.errors[:actions]).to include(error)
+        end
+      end
+
+      context 'with actions' do
+        subject(:auxiliary_instance) {
+          FactoryGirl.build(
+              :metasploit_cache_auxiliary_instance,
+              actions: []
+          ).tap { |actionable|
+            actionable.actions << FactoryGirl.build(
+                :metasploit_cache_auxiliary_action,
+                actionable: actionable
+            )
+          }
+        }
+
+        it 'does not adds error on #actions' do
+          auxiliary_instance.valid?
+
+          expect(auxiliary_instance.errors[:actions]).not_to include(error)
+        end
+      end
+    end
   end
 end
