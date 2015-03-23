6fdf706130050d3f36d35940bfef639ca9583613
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-23 14:28:39 -0500

    Red Metasploit::Cache::Auxiliary::Instance#description spec
    MSP-12352

--- spec/app/models/metasploit/cache/auxiliary/instance_spec.rb
+++ spec/app/models/metasploit/cache/auxiliary/instance_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Metasploit::Cache::Auxiliary::Instance, type: :model do
   context 'database' do
     context 'columns' do
       it { is_expected.to have_db_column(:default_action_id).of_type(:integer).with_options(null: true) }
+      it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
       it { is_expected.to have_db_column(:auxiliary_class_id).of_type(:integer).with_options(null: false) }
     end
 
@@ -221,5 +222,7 @@ RSpec.describe Metasploit::Cache::Auxiliary::Instance, type: :model do
         end
       end
     end
+
+    it { is_expected.to validate_presence_of :description }
   end
 end
