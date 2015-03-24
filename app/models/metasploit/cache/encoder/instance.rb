80ea618a9393c6ede68a8998a6466a0fa824ad60
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-24 13:04:03 -0500

    Green Encoder::Instance Metasploit::Concern.run
    MSP-12366

--- app/models/metasploit/cache/encoder/instance.rb
+++ app/models/metasploit/cache/encoder/instance.rb
@@ -1,4 +1,30 @@
 # Instance-level metadata for an encoder  Metasploit Module.
-class Metasploit::Cache::Encoder::Instance
+class Metasploit::Cache::Encoder::Instance < ActiveRecord::Base
+  #
+  # Attributes
+  #
+
+  # @!attribute description
+  # The long-form human-readable description of this auxiliary Metasploit Module.
+  #
+  #   @return [String]
+
+  #
+  # Validations
+  #
+
+  validates :description,
+            presence: true
+
+  #
+  # Instance Methods
+  #
+
+  # @!method description=(description)
+  #   Sets {#description}.
+  #
+  #   @param description [String] The long-form human-readable description of this encoder Metasploit Module.
+  #   @return [void]
+
   Metasploit::Concern.run(self)
 end
--- lib/metasploit/cache/encoder.rb
+++ lib/metasploit/cache/encoder.rb
@@ -7,4 +7,15 @@ module Metasploit::Cache::Encoder
   autoload :Ancestor
   autoload :Class
   autoload :Instance
+
+  #
+  # Module Methods
+  #
+
+  # The prefix for ActiveRecord::Base subclass table names in this namespace.
+  #
+  # @return [String]
+  def self.table_name_prefix
+    "#{parent.table_name_prefix}encoder_"
+  end
 end
