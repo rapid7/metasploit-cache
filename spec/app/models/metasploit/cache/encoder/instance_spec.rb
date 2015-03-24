81b39435fcd7e16c446f6d1c437944edd160bcd0
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-24 13:25:50 -0500

    Red Encoder::Instance#encoder_class and Encoder::Class#encoder_instance
    MSP-12366

--- spec/app/models/metasploit/cache/encoder/instance_spec.rb
+++ spec/app/models/metasploit/cache/encoder/instance_spec.rb
@@ -1,15 +1,24 @@
 RSpec.describe Metasploit::Cache::Encoder::Instance do
   it_should_behave_like 'Metasploit::Concern.run'
 
+  context 'associations' do
+    it { belongs_to(:encoder_class).class_name('Metasploit::Cache::Encoder::Class').inverse_of(:encoder_instance) }
+  end
+
   context 'database' do
     context 'columns' do
       it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
       it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
     end
+
+    context 'indices' do
+      it { is_expected.to have_db_index(:encoder_class_id).unique(true) }
+    end
   end
 
   context 'validations' do
     it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :encoder_class }
     it { is_expected.to validate_presence_of :name }
   end
 end
