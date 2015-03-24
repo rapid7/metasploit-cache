bb1ca5fde322319bb7f37ba147d45893810c6602
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-24 12:45:40 -0500

    Red Encoder::Instance#description
    MSP-12366

--- spec/app/models/metasploit/cache/encoder/instance_spec.rb
+++ spec/app/models/metasploit/cache/encoder/instance_spec.rb
@@ -1,3 +1,13 @@
 RSpec.describe Metasploit::Cache::Encoder::Instance do
   it_should_behave_like 'Metasploit::Concern.run'
+
+  context 'database' do
+    context 'columns' do
+      it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
+    end
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of :description }
+  end
 end
