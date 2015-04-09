f2220148559ab61f6eeae010ac0507720a2cc57c
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-09 12:31:47 -0500

    Add missing YARD docs
    MSP-12423

--- app/models/metasploit/cache/nop/instance.rb
+++ app/models/metasploit/cache/nop/instance.rb
@@ -24,6 +24,11 @@ class Metasploit::Cache::Nop::Instance < ActiveRecord::Base
   #
   #   @return [String]
 
+  # @!attribute nop_class_id
+  #   The foreign key for the {#nop_class} association.
+  #
+  #   @return [Integer]
+
   #
   # Validations
   #
@@ -52,6 +57,12 @@ class Metasploit::Cache::Nop::Instance < ActiveRecord::Base
   #
   #   @param name [String] The human-readable name of this exploit Metasploit Module.  This can be thought of as the
   #     title or summary of the Metasploit Module.
+  #   @return [void]
+
+  # @!method nop_class_id=(nop_class_id)
+  #   Sets {#nop_class_id} and causes cache of {#nop_class} to be invalidated and reloaded on next access.
+  #
+  #   @param nop_class_id [Integer]
   #   @return [void]
 
   Metasploit::Concern.run(self)
