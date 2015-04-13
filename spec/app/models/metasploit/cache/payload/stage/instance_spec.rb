423bf5c1dbbdc092d3bcb7b82c962f7ff9b96323
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-13 14:46:26 -0500

    Red Payload::Stage::Instance#name
    MSP-12425

--- spec/app/models/metasploit/cache/payload/stage/instance_spec.rb
+++ spec/app/models/metasploit/cache/payload/stage/instance_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe Metasploit::Cache::Payload::Stage::Instance do
   context 'database' do
     context 'columns' do
       it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
+      it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
     end
   end
 
   context 'validations' do
     it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :name }
   end
 end
