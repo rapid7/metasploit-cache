186ac56159767ffc3bd092ab584ee0bae6d45ad4
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-13 15:13:43 -0500

    Fix typo
    MSP-12425

--- lib/metasploit/cache/payload/stage.rb
+++ lib/metasploit/cache/payload/stage.rb
@@ -1,6 +1,6 @@
 # Namespace for stage payload Metasploit Module cache metadata, including
 # {Metasploit::Cache::Payload::Stage::Ancestor ancestors}, {Metasploit::Cache::Payload::Stage::Class classes}, and
-# {Metasploit::Cache::Paylaod::Stage::Instance instances}.
+# {Metasploit::Cache::Payload::Stage::Instance instances}.
 module Metasploit::Cache::Payload::Stage
   extend ActiveSupport::Autoload
 
