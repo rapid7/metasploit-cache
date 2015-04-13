55b2f18b5253ab83b627d9d4f7dd630ad0bde87b
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-13 09:48:31 -0500

    Red Payload::Single::Instance#privileged
    MSP-12424

--- spec/app/models/metasploit/cache/payload/single/instance_spec.rb
+++ spec/app/models/metasploit/cache/payload/single/instance_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe Metasploit::Cache::Payload::Single::Instance do
     context 'columns' do
       it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
       it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
+      it { is_expected.to have_db_column(:privileged).of_type(:boolean).with_options(null: false) }
     end
   end
 
   context 'validations' do
     it { is_expected.to validate_presence_of :description }
     it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_inclusion_of(:privileged).in_array([false, true]) }
   end
 end
