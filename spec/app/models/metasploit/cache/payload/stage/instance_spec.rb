31512343280b577690da859a723dc1f1df854079
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-13 15:05:04 -0500

    Red Payload::Single::Class#payload_stage_instance and Payload::Single::Instance#payload_stage_class
    MSP-12425

--- spec/app/models/metasploit/cache/payload/stage/instance_spec.rb
+++ spec/app/models/metasploit/cache/payload/stage/instance_spec.rb
@@ -5,11 +5,39 @@ RSpec.describe Metasploit::Cache::Payload::Stage::Instance do
     context 'columns' do
       it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
       it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
+      it { is_expected.to have_db_column(:payload_stage_class_id).of_type(:integer).with_options(null: false) }
+    end
+
+    context 'indices' do
+      it { is_expected.to have_db_index(:payload_stage_class_id).unique(true) }
+    end
+  end
+
+  context 'factories' do
+    context 'metasploit_cache_payload_stage_instance' do
+      subject(:metasploit_cache_payload_stage_instance) {
+        FactoryGirl.build(:metasploit_cache_payload_stage_instance)
+      }
+
+      it { is_expected.to be_valid }
     end
   end
 
   context 'validations' do
     it { is_expected.to validate_presence_of :description }
     it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :payload_stage_class }
+
+    # validate_uniqueness_of needs a pre-existing record of the same class to work correctly when the `null: false`
+    # constraints exist for other fields.
+    context 'with existing record' do
+      let!(:existing_payload_stage_instance) {
+        FactoryGirl.create(
+            :metasploit_cache_payload_stage_instance
+        )
+      }
+
+      it { is_expected.to validate_uniqueness_of :payload_stage_class_id }
+    end
   end
 end
