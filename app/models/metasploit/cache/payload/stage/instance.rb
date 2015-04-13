578e197400c4b781c2b78a8bd89a28f05a834269
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-13 15:01:29 -0500

    Green Payloa::Stage::Instance#name
    MSP-12425

--- app/models/metasploit/cache/payload/stage/instance.rb
+++ app/models/metasploit/cache/payload/stage/instance.rb
@@ -9,11 +9,19 @@ class Metasploit::Cache::Payload::Stage::Instance < ActiveRecord::Base
   #
   #   @return [String]
 
+  # @!attribute name
+  #   The human-readable name of this stage payload Metasploit Module.  This can be thought of as the title or summary
+  #   of the Metasploit Module.
+  #
+  #   @return [String]
+
   #
   # Validations
   #
 
   validates :description,
+            presence: true
+  validates :name,
             presence: true
 
   #
@@ -24,6 +32,13 @@ class Metasploit::Cache::Payload::Stage::Instance < ActiveRecord::Base
   #   Sets {#description}.
   #
   #   @param description [String] The long-form human-readable description of this stage payload Metasploit Module.
+  #   @return [void]
+
+  # @!method name=(name)
+  #   Sets {#name}.
+  #
+  #   @param name [String] The human-readable name of this stage payload Metasploit Module.  This can be thought of as
+  #     the title or summary of the Metasploit Module.
   #   @return [void]
 
   Metasploit::Concern.run(self)
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -361,7 +361,8 @@ ActiveRecord::Schema.define(:version => 20150413193613) do
   add_index "mc_payload_single_instances", ["payload_single_class_id"], :name => "index_mc_payload_single_instances_on_payload_single_class_id", :unique => true
 
   create_table "mc_payload_stage_instances", :force => true do |t|
-    t.text "description", :null => false
+    t.text   "description", :null => false
+    t.string "name",        :null => false
   end
 
   create_table "mc_platforms", :force => true do |t|
