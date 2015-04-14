dfd4febba4b29cf89b3bd1b3e9b09ea435cc7a4f
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-14 15:52:55 -0500

    Red Payload::Stager::Instance#name
    MSP-12426

--- spec/app/models/metasploit/cache/payload/stager/instance_spec.rb
+++ spec/app/models/metasploit/cache/payload/stager/instance_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe Metasploit::Cache::Payload::Stager::Instance do
     context 'columns' do
       it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
       it { is_expected.to have_db_column(:handler_type_alias).of_type(:string).with_options(null: true) }
+      it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
     end
   end
 
   context 'validations' do
     it { is_expected.to validate_presence_of :description }
     it { is_expected.not_to validate_presence_of :handler_type_alias }
+    it { is_expected.to validate_presence_of :name }
   end
 end
