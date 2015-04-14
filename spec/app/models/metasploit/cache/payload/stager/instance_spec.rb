c6a2e048e4b420d27a927915ab9dfc37a6972130
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-14 15:41:57 -0500

    Red Payload::Stager::Instance#handler_type_alias
    MSP-12426

--- spec/app/models/metasploit/cache/payload/stager/instance_spec.rb
+++ spec/app/models/metasploit/cache/payload/stager/instance_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe Metasploit::Cache::Payload::Stager::Instance do
   context 'database' do
     context 'columns' do
       it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
+      it { is_expected.to have_db_column(:handler_type_alias).of_type(:string).with_options(null: true) }
     end
   end
 
   context 'validations' do
     it { is_expected.to validate_presence_of :description }
+    it { is_expected.not_to validate_presence_of :handler_type_alias }
   end
 end
