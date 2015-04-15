6a4d887138f3f290a4906b78ffaa39fbc947a4d9
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-15 08:48:27 -0500

    Red Payload::Stager::Class#payload_stager_instance and Payload::Stager::Instance#payload_stager_class
    MSP-12426

--- spec/app/models/metasploit/cache/payload/stager/instance_spec.rb
+++ spec/app/models/metasploit/cache/payload/stager/instance_spec.rb
@@ -6,12 +6,40 @@ RSpec.describe Metasploit::Cache::Payload::Stager::Instance do
       it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
       it { is_expected.to have_db_column(:handler_type_alias).of_type(:string).with_options(null: true) }
       it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
+      it { is_expected.to have_db_column(:payload_stager_class_id).of_type(:integer).with_options(null: false) }
+    end
+
+    context 'indices' do
+      it { is_expected.to have_db_index(:payload_stager_class_id).unique(true) }
     end
   end
 
+  context 'factories' do
+    context 'metasploit_cache_payload_stager_instance' do
+      subject(:metasploit_cache_payload_stager_instance) {
+        FactoryGirl.build(:metasploit_cache_payload_stager_instance)
+      }
+
+      it { is_expected.to be_valid }
+    end
+  end
+  
   context 'validations' do
     it { is_expected.to validate_presence_of :description }
     it { is_expected.not_to validate_presence_of :handler_type_alias }
     it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :payload_stager_class }
+
+    # validate_uniqueness_of needs a pre-existing record of the same class to work correctly when the `null: false`
+    # constraints exist for other fields.
+    context 'with existing record' do
+      let!(:existing_payload_stager_instance) {
+        FactoryGirl.create(
+            :metasploit_cache_payload_stager_instance
+        )
+      }
+
+      it { is_expected.to validate_uniqueness_of :payload_stager_class_id }
+    end
   end
 end
