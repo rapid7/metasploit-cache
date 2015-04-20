8712951bedb8a91d965ccdfcf8b14383d3a103be
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-20 08:30:40 -0500

    Update version for branch

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -11,7 +11,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 62
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 3
+      PATCH = 4
+      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
+      PRERELEASE = 'sqlite3'
 
       #
       # Module Methods
