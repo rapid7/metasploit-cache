7937bc4a87475009e10629ea209d18775e4a2af7
Trevor Rosen <trevor_rosen@rapid7.com>
2015-04-22 13:41:27 -0500

    Remove PRERELEASE

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -12,8 +12,6 @@ module Metasploit
       MINOR = 64
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 5
-      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'load-encoder-class-from-framework'
 
       #
       # Module Methods
