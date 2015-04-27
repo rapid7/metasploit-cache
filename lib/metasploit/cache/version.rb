2dfe7653c1e36c92f14a48c6c8966bc7bde6779f
Trevor Rosen <trevor_rosen@rapid7.com>
2015-04-27 14:16:24 -0500

    Remove PRERELEASE

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -12,9 +12,7 @@ module Metasploit
       MINOR = 64
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 9
-      # Remove on master
-      PRERELEASE='load-nop-class-from-framework'
-      #
+
       # Module Methods
       #
 
