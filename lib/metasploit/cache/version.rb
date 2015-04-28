d04b7b29a20c9e466f26fe524c119e457499c12d
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-28 09:08:32 -0500

    Update version for branch
    MSP-12427

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -11,7 +11,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 64
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 12
+      PATCH = 13
+      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
+      PRERELEASE = 'post-instance'
 
       #
       # Module Methods
