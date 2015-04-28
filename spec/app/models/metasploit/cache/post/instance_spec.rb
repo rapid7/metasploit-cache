6304cd94f6054dbe091aab4264987172eb35f779
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-28 09:33:24 -0500

    Red Post::Instance#disclosed_on
    MSP-12427

--- spec/app/models/metasploit/cache/post/instance_spec.rb
+++ spec/app/models/metasploit/cache/post/instance_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe Metasploit::Cache::Post::Instance do
   context 'database' do
     context 'columns' do
       it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
+      it { is_expected.to have_db_column(:disclosed_on).of_type(:date).with_options(null: false) }
     end
   end
 
   context 'validations' do
     it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :disclosed_on }
   end
 end
