c930c3781c2388ff9e5924a43b56c7b8985ba66a
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-28 11:15:15 -0500

    Remove other branch schema leakage
    MSP-12427

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -60,17 +60,6 @@ ActiveRecord::Schema.define(:version => 20150428142801) do
   add_index "mc_email_addresses", ["full"], :name => "index_mc_email_addresses_on_full", :unique => true
   add_index "mc_email_addresses", ["local"], :name => "index_mc_email_addresses_on_local"
 
-  create_table "mc_exploit_instances", :force => true do |t|
-    t.text    "description",      :null => false
-    t.date    "disclosed_on",     :null => false
-    t.string  "name",             :null => false
-    t.boolean "privileged",       :null => false
-    t.string  "stance",           :null => false
-    t.integer "exploit_class_id", :null => false
-  end
-
-  add_index "mc_exploit_instances", ["exploit_class_id"], :name => "index_mc_exploit_instances_on_exploit_class_id", :unique => true
-
   create_table "mc_module_actions", :force => true do |t|
     t.integer "module_instance_id", :null => false
     t.text    "name",               :null => false
