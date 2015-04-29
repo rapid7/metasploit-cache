09ca7b77903b6e4a600db19c394cfc358b1312d4
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-29 09:49:12 -0500

    Green Payload::Handler Metasploit::Concern.run
    MSP-12433

--- app/models/metasploit/cache/payload/handler.rb
+++ app/models/metasploit/cache/payload/handler.rb
@@ -1,4 +1,5 @@
 # Handles the local connection for a {Metasploit::Cache::Payload::Single::Instance single payload Metasploit Module} or
 # {Metasploit::Cache::Payload::Stager::Instance stager payload Metasploit Module}.
 class Metasploit::Cache::Payload::Handler
+  Metasploit::Concern.run(self)
 end
