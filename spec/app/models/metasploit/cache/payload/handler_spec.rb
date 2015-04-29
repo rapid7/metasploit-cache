650a7a5734ec7ba4dd65c52659d12de8de83e47d
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-29 13:16:07 -0500

    Red Payload::Handler#general_handler_type
    MSP-12433

--- spec/app/models/metasploit/cache/payload/handler_spec.rb
+++ spec/app/models/metasploit/cache/payload/handler_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe Metasploit::Cache::Payload::Handler do
   context 'database' do
     context 'columns' do
+      it { is_expected.to have_db_column(:general_handler_type).of_type(:string).with_options(null: false) }
       it { is_expected.to have_db_column(:handler_type).of_type(:string).with_options(null: false) }
     end
 
@@ -9,9 +10,31 @@ RSpec.describe Metasploit::Cache::Payload::Handler do
     end
   end
 
+  context 'factories' do
+    context 'metasploit_cache_payload_handler' do
+      subject(:metasploit_cache_payload_handler) {
+        FactoryGirl.build(:metasploit_cache_payload_handler)
+      }
+
+      it { is_expected.to be_valid }
+    end
+  end
+
   context 'validations' do
+    it { is_expected.to validate_inclusion_of(:general_handler_type).in_array(Metasploit::Cache::Payload::Handler::GeneralType::ALL) }
     it { is_expected.to validate_presence_of :handler_type }
-    it { is_expected.to validate_uniqueness_of :handler_type }
+
+    # validate_uniqueness_of needs a pre-existing record of the same class to work correctly when the `null: false`
+    # constraints exist for other fields.
+    context 'with existing record' do
+      let!(:existing_payload_handler) {
+        FactoryGirl.create(
+            :metasploit_cache_payload_handler
+        )
+      }
+
+      it { is_expected.to validate_uniqueness_of :handler_type }
+    end
   end
 
   it_should_behave_like 'Metasploit::Concern.run'
