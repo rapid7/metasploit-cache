1682f400c2b506fb21ecefe3d7f0ee2c64c1bbfd
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-29 13:10:24 -0500

    Green Payload::Handler::GeneralType
    MSP-12433

--- app/models/metasploit/cache/payload/handler/general_type.rb
+++ app/models/metasploit/cache/payload/handler/general_type.rb
@@ -1,2 +1,31 @@
+# Namespace for valid `Metasploit::Cache::Payload::Handler#general_type`s.
 module Metasploit::Cache::Payload::Handler::GeneralType
+  #
+  # CONSTANTS
+  #
+
+  # Binds to a remote socket
+  BIND = 'bind'
+
+  # Find a pre-existing socket either directly or through some remote service
+  FIND = 'find'
+
+  # Payloads that have no handler because they have no connection
+  NONE = 'none'
+
+  # Connect back to a local socket
+  REVERSE = 'reverse'
+
+  # Connection is tunnelled through another protocol, such as HTTP(S).  All {TUNNEL} handlers are also {REVERSE}
+  # currently.
+  TUNNEL = 'tunnel'
+
+  # All valid values for `Metasploit::Cache::Payload::Handler#general_type`.
+  ALL = [
+      BIND,
+      FIND,
+      NONE,
+      REVERSE,
+      TUNNEL
+  ]
 end
