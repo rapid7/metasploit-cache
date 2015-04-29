58991d26d41c4c5c243a4d760f8743959cdbaafd
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-29 10:45:29 -0500

    Red Payload::Handler#handler_type
    MSP-12433
    
    handler_type is a Module method for handlers in metasploit-framework.
    It matches the underscored relative Module#name.

--- spec/app/models/metasploit/cache/payload/handler_spec.rb
+++ spec/app/models/metasploit/cache/payload/handler_spec.rb
@@ -1,3 +1,18 @@
 RSpec.describe Metasploit::Cache::Payload::Handler do
+  context 'database' do
+    context 'columns' do
+      it { is_expected.to have_db_column(:handler_type).of_type(:string).with_options(null: false) }
+    end
+
+    context 'indices' do
+      it { is_expected.to have_db_indice(:handler_type).unique(true) }
+    end
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of :handler_type }
+    it { is_expected.to validate_uniqueness_of :handler_type }
+  end
+
   it_should_behave_like 'Metasploit::Concern.run'
 end
