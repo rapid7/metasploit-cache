f2b7e39f6dd452d039716b021620d8d368b99206
Trevor Rosen <trevor_rosen@rapid7.com>
2015-05-01 13:54:28 -0500

    Remove PRERELEASE

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -12,8 +12,6 @@ module Metasploit
       MINOR = 64
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 25
-      # Remove on master
-      PRERELEASE = "payload-stager-instance"
 
 
       #
