dada8365956c89f8c7b323e49f9b3a1734c09fae
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-07 14:28:00 -0500

    Red Post::Instance#default_action
    MSP-12429

--- spec/app/models/metasploit/cache/post/instance_spec.rb
+++ spec/app/models/metasploit/cache/post/instance_spec.rb
@@ -3,11 +3,13 @@ RSpec.describe Metasploit::Cache::Post::Instance do
 
   context 'associations' do
     it { is_expected.to have_many(:actions).class_name('Metasploit::Cache::Actionable::Action').inverse_of(:actionable) }
+    it { is_expected.to belong_to(:default_action).class_name('Metasploit::Cache::Actionable::Action').inverse_of(:actionable) }
     it { is_expected.to belong_to(:post_class).class_name('Metasploit::Cache::Post::Class').inverse_of(:post_instance) }
   end
 
   context 'database' do
     context 'columns' do
+      it { is_expected.to have_db_column(:default_action_id).of_type(:integer).with_options(null: true) }
       it { is_expected.to have_db_column(:description).of_type(:text).with_options(null: false) }
       it { is_expected.to have_db_column(:disclosed_on).of_type(:date).with_options(null: false) }
       it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
@@ -16,6 +18,7 @@ RSpec.describe Metasploit::Cache::Post::Instance do
     end
 
     context 'indices' do
+      it { is_expected.to have_db_index(:default_action_id).unique(true) }
       it { is_expected.to have_db_index(:post_class_id).unique(true) }
     end
   end
@@ -36,6 +39,66 @@ RSpec.describe Metasploit::Cache::Post::Instance do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :post_class }
     it { is_expected.to validate_inclusion_of(:privileged).in_array([false, true]) }
+
+    context 'validates inclusion of #default_action in #actions' do
+      subject(:default_action_errors) {
+        post_instance.errors[:default_action]
+      }
+
+      let(:error) {
+        I18n.translate!('activerecord.errors.models.metasploit/cache/post/instance.attributes.default_action.inclusion')
+      }
+
+      let(:post_instance) {
+        described_class.new
+      }
+
+      context 'without #default_action' do
+        before(:each) do
+          post_instance.default_action = nil
+        end
+
+        it { is_expected.not_to include(error) }
+      end
+
+      context 'with #default_action' do
+        #
+        # lets
+        #
+
+        let(:default_action) {
+          Metasploit::Cache::Actionable::Action.new
+        }
+
+        #
+        # Callbacks
+        #
+
+        before(:each) do
+          post_instance.default_action = default_action
+        end
+
+        context 'in #actions' do
+          before(:each) do
+            post_instance.actions = [
+                default_action
+            ]
+            post_instance.valid?
+          end
+
+          it { is_expected.not_to include(error) }
+        end
+
+        context 'not in #actions' do
+          before(:each) do
+            post_instance.actions = []
+            post_instance.valid?
+          end
+
+          it { is_expected.to include(error) }
+        end
+      end
+    end
 
     # validate_uniqueness_of needs a pre-existing record of the same class to work correctly when the `null: false`
     # constraints exist for other fields.
