e4af59cc4d1595e576fbdc97afead70b7308e1b2
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-19 12:48:04 -0500

    Red Encoder::Instance#architecturable_architectures length
    MSP-12431

--- spec/app/models/metasploit/cache/encoder/instance_spec.rb
+++ spec/app/models/metasploit/cache/encoder/instance_spec.rb
@@ -32,5 +32,45 @@ RSpec.describe Metasploit::Cache::Encoder::Instance do
     it { is_expected.to validate_presence_of :description }
     it { is_expected.to validate_presence_of :encoder_class }
     it { is_expected.to validate_presence_of :name }
+
+    # validate_length_of from shoulda-matchers assumes attribute is String and doesn't work on associations
+    context 'validates length of architecturable_architectures is at least 1' do
+      let(:error) {
+        I18n.translate!(
+          'activerecord.errors.models.metasploit/cache/encoder/instance.attributes.architecturable_architectures.too_short',
+           count: 1
+        )
+      }
+
+      context 'without architecturable_architectures' do
+        subject(:encoder_instance) {
+          FactoryGirl.build(
+              :metasploit_cache_encoder_instance,
+              architecturable_architecture_count: 0
+          )
+        }
+
+        it 'adds error on #architecturable_architectures' do
+          encoder_instance.valid?
+
+          expect(encoder_instance.errors[:architecturable_architectures]).to include(error)
+        end
+      end
+
+      context 'with architecturable_architectures' do
+        subject(:encoder_instance) {
+          FactoryGirl.build(
+              :metasploit_cache_encoder_instance,
+              architecturable_architecture_count: 1
+          )
+        }
+
+        it 'does not adds error on #architcturable_architectures' do
+          encoder_instance.valid?
+
+          expect(encoder_instance.errors[:architecturable_architectures]).not_to include(error)
+        end
+      end
+    end
   end
 end
