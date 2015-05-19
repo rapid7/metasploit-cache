f78dfa9ac638b3d06b8c6004c9e5e035680c44b6
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-19 13:48:57 -0500

    Use 'validates at least one associated' for Auxiliary::Instance#actions
    MSP-12431
    
    Had to rename transient actions_count to action_count as it differed
    from normal naming scheme of <singular>_count.

--- spec/app/models/metasploit/cache/auxiliary/instance_spec.rb
+++ spec/app/models/metasploit/cache/auxiliary/instance_spec.rb
@@ -35,45 +35,9 @@ RSpec.describe Metasploit::Cache::Auxiliary::Instance, type: :model do
   context 'validations' do
     it { is_expected.to validate_presence_of(:auxiliary_class) }
 
-    # validate_lenght_of from shoulda-matchers assumes attribute is String and doesn't work on associations
-    context 'validates length of actions is at least 1' do
-      let(:error) {
-        I18n.translate!(
-          'activerecord.errors.models.metasploit/cache/auxiliary/instance.attributes.actions.too_short',
-           count: 1
-        )
-      }
-
-      context 'without actions' do
-        subject(:auxiliary_instance) {
-          FactoryGirl.build(
-              :metasploit_cache_auxiliary_instance,
-              actions_count: 0
-          )
-        }
-
-        it 'adds error on #actions' do
-          auxiliary_instance.valid?
-
-          expect(auxiliary_instance.errors[:actions]).to include(error)
-        end
-      end
-
-      context 'with actions' do
-        subject(:auxiliary_instance) {
-          FactoryGirl.build(
-              :metasploit_cache_auxiliary_instance,
-              actions_count: 1
-          )
-        }
-
-        it 'does not adds error on #actions' do
-          auxiliary_instance.valid?
-
-          expect(auxiliary_instance.errors[:actions]).not_to include(error)
-        end
-      end
-    end
+    it_should_behave_like 'validates at least one associated',
+                          :actions,
+                          factory: :metasploit_cache_auxiliary_instance
 
     context 'actions_contains_default_action' do
       let(:error) {
@@ -88,7 +52,7 @@ RSpec.describe Metasploit::Cache::Auxiliary::Instance, type: :model do
             subject(:auxiliary_instance) {
               FactoryGirl.build(
                              :metasploit_cache_auxiliary_instance,
-                             actions_count: 1
+                             action_count: 1
               ).tap { |auxiliary_instance|
                 auxiliary_instance.default_action = auxiliary_instance.actions.first
               }
@@ -117,7 +81,7 @@ RSpec.describe Metasploit::Cache::Auxiliary::Instance, type: :model do
             subject(:auxiliary_instance) {
               FactoryGirl.build(
                              :metasploit_cache_auxiliary_instance,
-                             actions_count: 1
+                             action_count: 1
               ).tap { |auxiliary_instance|
                 auxiliary_instance.default_action = FactoryGirl.build(
                     :metasploit_cache_auxiliary_action,
@@ -150,7 +114,7 @@ RSpec.describe Metasploit::Cache::Auxiliary::Instance, type: :model do
           subject(:auxiliary_instance) {
             FactoryGirl.build(
                 :metasploit_cache_auxiliary_instance,
-                actions_count: 1
+                action_count: 1
             ).tap { |auxiliary_instance|
               auxiliary_instance.default_action = nil
             }
@@ -177,7 +141,7 @@ RSpec.describe Metasploit::Cache::Auxiliary::Instance, type: :model do
           subject(:auxiliary_instance) {
             FactoryGirl.build(
                 :metasploit_cache_auxiliary_instance,
-                actions_count: 0
+                action_count: 0
             ).tap { |auxiliary_instance|
               auxiliary_instance.default_action = FactoryGirl.build(
                   :metasploit_cache_auxiliary_action,
@@ -205,7 +169,7 @@ RSpec.describe Metasploit::Cache::Auxiliary::Instance, type: :model do
           subject(:auxiliary_instance) {
             FactoryGirl.build(
                 :metasploit_cache_auxiliary_instance,
-                actions_count: 0
+                action_count: 0
             ).tap { |auxiliary_instance|
               auxiliary_instance.default_action = nil
             }
--- spec/factories/metasploit/cache/auxiliary/instances.rb
+++ spec/factories/metasploit/cache/auxiliary/instances.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
   factory :metasploit_cache_auxiliary_instance,
           class: Metasploit::Cache::Auxiliary::Instance do
     transient do
-      actions_count 1
+      action_count 1
     end
 
     description { generate :metasploit_cache_auxiliary_instance_description }
@@ -26,7 +26,7 @@ FactoryGirl.define do
     after(:build) { |auxiliary_instance, evaluator|
       auxiliary_instance.actions = build_list(
           :metasploit_cache_auxiliary_action,
-          evaluator.actions_count,
+          evaluator.action_count,
           actionable: auxiliary_instance
       )
     }
