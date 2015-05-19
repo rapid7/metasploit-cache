ddb8f927f3296b662cba386b7003de989936607a
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-19 14:23:52 -0500

    Red Payload::Single::Instance#architecurable_architectures count
    MSP-12431

--- spec/app/models/metasploit/cache/payload/single/instance_spec.rb
+++ spec/app/models/metasploit/cache/payload/single/instance_spec.rb
@@ -39,6 +39,10 @@ RSpec.describe Metasploit::Cache::Payload::Single::Instance do
     it { is_expected.to validate_presence_of :payload_single_class }
     it { is_expected.to validate_inclusion_of(:privileged).in_array([false, true]) }
 
+    it_should_behave_like 'validates at least one associated',
+                          :architecturable_architectures,
+                          factory: :metasploit_cache_payload_single_instance
+
     # validate_uniqueness_of needs a pre-existing record of the same class to work correctly when the `null: false`
     # constraints exist for other fields.
     context 'with existing record' do
