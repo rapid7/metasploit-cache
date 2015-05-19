7af18c24056d7ee693d412dc7b6ee3b56a31b47c
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-19 11:04:14 -0500

    Green metasploit_cache_exploit_target_architecture factory
    MSP-12431

--- spec/factories/metasploit/cache/architecturable/architectures.rb
+++ spec/factories/metasploit/cache/architecturable/architectures.rb
@@ -11,6 +11,14 @@ FactoryGirl.define do
     association :architecturable, factory: :metasploit_cache_encoder_instance
   end
 
+  factory :metasploit_cache_exploit_target_architecture,
+          class: Metasploit::Cache::Architecturable::Architecture,
+          traits: [
+              :metasploit_cache_architecturable_architecture
+          ] do
+    association :architecturable, factory: :metasploit_cache_exploit_target
+  end
+
   #
   # Traits
   #
