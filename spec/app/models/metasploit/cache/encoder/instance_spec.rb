951dc2d92bab5d9d2418b19779c7a9c340d985dd
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-19 13:30:09 -0500

    Extract 'validates at least one associated' shared examples
    MSP-12431
    
    shoulda-matcher's `validates_length_of` only works on strings, so a
    custom context with positive and negative tests needed to be written for
    each has_many association that needs at least one member, but since the
    naming pattern in factories, error messages, and associations is
    consistent, this custom context can be generalized.

--- spec/app/models/metasploit/cache/encoder/instance_spec.rb
+++ spec/app/models/metasploit/cache/encoder/instance_spec.rb
@@ -33,44 +33,8 @@ RSpec.describe Metasploit::Cache::Encoder::Instance do
     it { is_expected.to validate_presence_of :encoder_class }
     it { is_expected.to validate_presence_of :name }
 
-    # validate_length_of from shoulda-matchers assumes attribute is String and doesn't work on associations
-    context 'validates length of architecturable_architectures is at least 1' do
-      let(:error) {
-        I18n.translate!(
-          'activerecord.errors.models.metasploit/cache/encoder/instance.attributes.architecturable_architectures.too_short',
-           count: 1
-        )
-      }
-
-      context 'without architecturable_architectures' do
-        subject(:encoder_instance) {
-          FactoryGirl.build(
-              :metasploit_cache_encoder_instance,
-              architecturable_architecture_count: 0
-          )
-        }
-
-        it 'adds error on #architecturable_architectures' do
-          encoder_instance.valid?
-
-          expect(encoder_instance.errors[:architecturable_architectures]).to include(error)
-        end
-      end
-
-      context 'with architecturable_architectures' do
-        subject(:encoder_instance) {
-          FactoryGirl.build(
-              :metasploit_cache_encoder_instance,
-              architecturable_architecture_count: 1
-          )
-        }
-
-        it 'does not adds error on #architcturable_architectures' do
-          encoder_instance.valid?
-
-          expect(encoder_instance.errors[:architecturable_architectures]).not_to include(error)
-        end
-      end
-    end
+    it_should_behave_like 'validates at least one associated',
+                          :architecturable_architectures,
+                          factory: :metasploit_cache_encoder_instance
   end
 end
