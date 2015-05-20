d3700121dd5cfc2e9896a34bd7f08205c5002eaa
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-20 11:14:33 -0500

    Green metasploit_cache_exploit_target_platform factory
    MSP-12432

--- spec/factories/metasploit/cache/platformable/platforms.rb
+++ spec/factories/metasploit/cache/platformable/platforms.rb
@@ -11,6 +11,14 @@ FactoryGirl.define do
     association :platformable, factory: :metasploit_cache_encoder_instance
   end
 
+  factory :metasploit_cache_exploit_target_platform,
+          class: Metasploit::Cache::Platformable::Platform,
+          traits: [
+              :metasploit_cache_platformable_platform
+          ] do
+    association :platformable, factory: :metasploit_cache_exploit_target
+  end
+
   #
   # Traits
   #
