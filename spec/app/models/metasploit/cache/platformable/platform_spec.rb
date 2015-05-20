6a73a7c6973dc13adfafc8d5b76fab2c41c71602
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-20 09:45:02 -0500

    Red Platformable::Platform#platform and inverse
    MSP-12432

--- spec/app/models/metasploit/cache/platformable/platform_spec.rb
+++ spec/app/models/metasploit/cache/platformable/platform_spec.rb
@@ -1,3 +1,21 @@
 RSpec.describe Metasploit::Cache::Platformable::Platform do
   it_should_behave_like 'Metasploit::Concern.run'
+
+  context 'associations' do
+    it { is_expected.to belong_to(:platform).class_name('Metasploit::Cache::Platform').inverse_of(:platformable_platforms) }
+  end
+
+  context 'database' do
+    context 'columns' do
+      it { is_expected.to have_db_column(:platform_id).of_type(:integer).with_options(null: false) }
+    end
+
+    context 'indices' do
+      it { is_expected.to have_db_index(:platform_id) }
+    end
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of :platform }
+  end
 end
