ce9a8b67e3b0a8050d9f38019285b1d1a72e6361
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-26 15:42:30 -0500

    Red Contribution#author and inverse
    MSP-12436

--- spec/app/models/metasploit/cache/contribution_spec.rb
+++ spec/app/models/metasploit/cache/contribution_spec.rb
@@ -1,3 +1,17 @@
 RSpec.describe Metasploit::Cache::Contribution do
   it_should_behave_like 'Metasploit::Concern.run'
+
+  context 'associations' do
+    it { is_expected.to belong_to(:author).class_name('Metasploit::Cache::Author').inverse_of(:contributions) }
+  end
+
+  context 'database' do
+    context 'columns' do
+      it { is_expected.to have_db_column(:author_id).of_type(:integer).with_options(null: false) }
+    end
+
+    context 'indices' do
+      it { is_expected.to have_db_index(:author_id).unique(false) }
+    end
+  end
 end
