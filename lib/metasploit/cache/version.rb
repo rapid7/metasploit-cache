902c97fe5858fa22e514412a5de12b093998f87f
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-27 13:09:44 -0500

    Update version for branch
    MSP-12436
    
    Incompatible change because some associations were repurposed.

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -9,9 +9,11 @@ module Metasploit
       # The major version number.
       MAJOR = 0
       # The minor version number, scoped to the {MAJOR} version number.
-      MINOR = 64
+      MINOR = 65
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 29
+      PATCH = 0
+      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
+      PRERELEASE = 'contribution'
 
       #
       # Module Methods
