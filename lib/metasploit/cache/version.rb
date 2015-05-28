b6c44f46dfc409a85df1c8fe57edda1db696fcd6
Trevor Rosen <trevor_rosen@rapid7.com>
2015-05-28 11:52:37 -0500

    Fix version

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -12,6 +12,8 @@ module Metasploit
       MINOR = 64
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 31
+      # Remove on master
+      PRERELEASE = 'extra-writer-docs'
 
       #
       # Module Methods
