779565207fcb0b2ed0e4cc3400e06cbc058d5806
Luke Imhoff <luke_imhoff@rapid7.com>
2015-07-15 12:27:46 -0500

    Update version for branch
    MSP-12440

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -11,9 +11,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 67
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 0
+      PATCH = 1
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version.
-      PRERELEASE = "auxiliary-instance-ephemeral-and-load"
+      PRERELEASE = 'load-auxiliary-instance-from-metasploit-framework'
 
       #
       # Module Methods
