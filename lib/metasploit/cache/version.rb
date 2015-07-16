b7ba87e47c086e1285da2bbb945dd4f46ffd9c32
Luke Imhoff <luke_imhoff@rapid7.com>
2015-07-16 10:51:43 -0500

    Update version for branch
    MSP-12438

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -11,9 +11,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 66
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 0
+      PATCH = 1
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version.
-      PRERELEASE = "rails-4-0"
+      PRERELEASE = 'payload-staged-instance'
 
       #
       # Module Methods
