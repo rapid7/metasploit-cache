7af141653fa1bd350bb2dd8f05446715572e9dc0
Luke Imhoff <luke_imhoff@rapid7.com>
2015-07-17 10:28:44 -0500

    Ephemeral::AttributeSet#existing_by_attribute_value
    MSP-12441
    
    Generalize logic used to get existing records from database using only a
    single attribute and store them locally in a Hash for faster lookup.

--- spec/lib/metasploit/cache/ephemeral/attribute_set_spec.rb
+++ spec/lib/metasploit/cache/ephemeral/attribute_set_spec.rb
@@ -20,6 +20,88 @@ RSpec.describe Metasploit::Cache::Ephemeral::AttributeSet do
     end
   end
 
+  context 'existing_by_attribute_value' do
+    subject(:existing_by_attribute_value) {
+      described_class.existing_by_attribute_value(
+          attribute: :abbreviation,
+          scope: Metasploit::Cache::Architecture,
+          value_set: value_set
+      )
+    }
+
+    #
+    # let!s
+    #
+
+    let!(:existing_not_in_value_set) {
+      FactoryGirl.generate :metasploit_cache_architecture
+    }
+
+    context 'with empty value_set' do
+      let(:value_set) {
+        Set.new
+      }
+
+      context 'with existing attribute value' do
+        it "still returns nil because value wasn't in :value_set" do
+          expect(existing_by_attribute_value[existing_not_in_value_set.abbreviation]).to be_nil
+        end
+      end
+
+      context 'without existing attribute value' do
+        let(:value) {
+          'asdf'
+        }
+
+        it 'returns nil' do
+          expect(existing_by_attribute_value[value]).to be_nil
+        end
+      end
+    end
+
+    context 'with present value_set' do
+      #
+      # let
+      #
+
+      let(:value_set) {
+        Set.new [existing_in_value_set.abbreviation]
+      }
+
+      #
+      # let!s
+      #
+
+      let!(:existing_in_value_set) {
+        FactoryGirl.generate :metasploit_cache_architecture
+      }
+
+      context 'with existing attribute value' do
+        context 'in value_set' do
+          it 'returns existing record' do
+            expect(existing_by_attribute_value[existing_in_value_set.abbreviation]).to eq(existing_in_value_set)
+          end
+        end
+
+        context 'not in value_set' do
+          it "still returns nil because attribute value wasn't in value_set" do
+            expect(existing_by_attribute_value[existing_not_in_value_set.abbreviation]).to be_nil
+          end
+        end
+      end
+
+      context 'without existing attribute value' do
+        let(:value) {
+          'asdf'
+        }
+
+        it 'returns nil' do
+          expect(existing_by_attribute_value[value]).to be_nil
+        end
+      end
+    end
+  end
+
   context 'removed' do
     subject(:removed) {
       described_class.removed(
