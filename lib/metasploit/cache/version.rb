4317fba08134b3ba71feeeebdcfa6e1337307f20
Luke Imhoff <luke_imhoff@rapid7.com>
2015-07-22 09:44:30 -0500

    Update version for branch
    MINOR version bump for renaming API.

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -9,11 +9,11 @@ module Metasploit
       # The major version number.
       MAJOR = 0
       # The minor version number, scoped to the {MAJOR} version number.
-      MINOR = 67
+      MINOR = 68
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 2
+      PATCH = 0
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version.
-      PRERELEASE = 'load-encoder-instance-from-metasploit-framework'
+      PRERELEASE = 'mark-for-destruction'
 
       #
       # Module Methods
