4dcebd8885a045b59aee445b63531215880a465c
Luke Imhoff <luke_imhoff@rapid7.com>
2015-07-22 09:33:22 -0500

    Red specs for architecturable_architecture mark_for_destruction
    MSP-13008
    
    `destroy_removed` should be renamed `marked_removed_for_destruction` and
    it should call `mark_for_destruction` on the removed
    `architecturable_architectures` instead of using a relation's
    `destroy_all` as `destroy_all` writes to the database violating design
    of `synchronize` only reading the `persist`'s call to `batched_save`
    doing all writing.

--- spec/app/models/metasploit/cache/encoder/instance_spec.rb
+++ spec/app/models/metasploit/cache/encoder/instance_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Metasploit::Cache::Encoder::Instance do
 
   context 'associations' do
     it { is_expected.to have_many(:architectures).class_name('Metasploit::Cache::Architecture') }
-    it { is_expected.to have_many(:architecturable_architectures).class_name('Metasploit::Cache::Architecturable::Architecture').dependent(:destroy).inverse_of(:architecturable) }
+    it { is_expected.to have_many(:architecturable_architectures).autosave(true).class_name('Metasploit::Cache::Architecturable::Architecture').dependent(:destroy).inverse_of(:architecturable) }
     it { is_expected.to have_many(:contributions).class_name('Metasploit::Cache::Contribution').dependent(:destroy).inverse_of(:contributable) }
     it { is_expected.to belong_to(:encoder_class).class_name('Metasploit::Cache::Encoder::Class').inverse_of(:encoder_instance) }
     it { is_expected.to have_many(:licensable_licenses).class_name('Metasploit::Cache::Licensable::License')}
