7bc036a00b0ebca55ce9a7195467bf3dcc03d353
Luke Imhoff <luke_imhoff@rapid7.com>
2015-07-24 12:32:14 -0500

    Update version for branch
    MSP-13028

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -11,8 +11,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 68
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 0
-
+      PATCH = 1
+      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
+      PRERELEASE = 'cell-comma-separation'
 
       #
       # Module Methods
