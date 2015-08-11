65c6a6adce6f176ea928f194dd117c909039fac5
Luke Imhoff <luke_imhoff@rapid7.com>
2015-08-11 13:48:45 -0500

    Update version for branch
    MSP-12448
    
    Bump MAJOR because the `:metasploit_cache_payload_single_instance` drops
    building the associations in favor of
    `:full_metasploit_cache_payload_single_instance` building all
    associations using traits.

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -9,11 +9,11 @@ module Metasploit
       # The major version number.
       MAJOR = 0
       # The minor version number, scoped to the {MAJOR} version number.
-      MINOR = 71
+      MINOR = 72
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 2
+      PATCH = 0
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'missing-batch-descendant'
+      PRERELEASE = 'payload-single-instance-ephemeral-and-load'
 
       #
       # Module Methods
