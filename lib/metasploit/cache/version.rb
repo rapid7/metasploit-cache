e06882af916cdcc94f9ecda4d0f1b589694b25c9
Luke Imhoff <luke_imhoff@rapid7.com>
2015-08-12 10:30:24 -0500

    Update version for branch
    MSP-12449
    
    MAJOR bump because consumer of "Metasploit::Cache::*::Instance::Load
    from relative_path_prefix" must define `direct_class_load` now.

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -9,11 +9,11 @@ module Metasploit
       # The major version number.
       MAJOR = 0
       # The minor version number, scoped to the {MAJOR} version number.
-      MINOR = 72
+      MINOR = 73
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 0
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'payload-single-instance-ephemeral-and-load'
+      PRERELEASE = 'load-payload-single-instance-from-metasploit-framework'
 
       #
       # Module Methods
