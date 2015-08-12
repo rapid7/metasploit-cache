9844e9993c4913e7f7b663554b9432d3afd08509
Luke Imhoff <luke_imhoff@rapid7.com>
2015-08-12 14:42:00 -0500

    Update version for branch
    MSP-12454
    
    Bump MINOR because changes to factories will be incompatible.

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -9,11 +9,11 @@ module Metasploit
       # The major version number.
       MAJOR = 0
       # The minor version number, scoped to the {MAJOR} version number.
-      MINOR = 73
+      MINOR = 74
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 0
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'load-payload-single-instance-from-metasploit-framework'
+      PRERELEASE = 'payload-stager-instance-ephemeral-and-load'
 
       #
       # Module Methods
