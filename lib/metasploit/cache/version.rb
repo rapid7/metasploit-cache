5ab13d09710df1d1dc594e1ec2558fc7f43100dd
Trevor Rosen <trevor_rosen@rapid7.com>
2015-08-17 14:51:30 -0500

    Fix version branch name

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -13,7 +13,7 @@ module Metasploit
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 0
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'payload-stage-instance-ephemeral-and-load'
+      PRERELEASE = 'load-payload-stage-instance-from-metasploit-framework'
 
       #
       # Module Methods
