84c6e7ff7f9263edf3aac912e0fe6e5e435e25bd
Luke Imhoff <luke_imhoff@rapid7.com>
2015-09-01 08:02:49 -0500

    Update version for branch
    MSP-13194

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -11,9 +11,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 76
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 0
+      PATCH = 1
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'remove-previous-schema-and-models'
+      PRERELEASE = 'pull-review-cleanup'
 
       #
       # Module Methods
