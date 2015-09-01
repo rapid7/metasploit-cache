3592e5b0f33b9909a332ae7860adae669d6d98e5
Luke Imhoff <luke_imhoff@rapid7.com>
2015-09-01 15:27:14 -0500

    Update version for branch
    MSP-9202

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -11,9 +11,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 76
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 1
+      PATCH = 2
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'pull-review-cleanup'
+      PRERELEASE = 'staged-payload-multiple-licenses'
 
       #
       # Module Methods
