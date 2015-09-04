9431038ef904013099718563f425524ecf6df9bd
Luke Imhoff <luke_imhoff@rapid7.com>
2015-09-04 10:27:02 -0500

    Check that Payload::Single::Handled::Class factory is loadable
    MSP-13201

--- spec/app/models/metasploit/cache/payload/handled/class_spec.rb
+++ spec/app/models/metasploit/cache/payload/handled/class_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe Metasploit::Cache::Payload::Single::Handled::Class do
 
   context 'factories' do
     context 'metasploit_cache_payload_single_handled_class' do
+      include_context 'ActiveSupport::TaggedLogging'
       include_context ':metasploit_cache_payload_handler_module'
 
       subject(:metasploit_cache_payload_single_handled_class) {
@@ -27,6 +28,99 @@ RSpec.describe Metasploit::Cache::Payload::Single::Handled::Class do
       }
 
       it { is_expected.to be_valid }
+
+      context 'loading' do
+        include_context 'ActiveSupport::TaggedLogging'
+        include_context 'Metasploit::Cache::Spec::Unload.unload'
+
+        #
+        # lets
+        #
+
+        let(:metasploit_framework) {
+          double('Metasploit Framework')
+        }
+
+        let(:payload_single_ancestor) {
+          payload_single_unhandled_class.ancestor
+        }
+
+
+        let(:payload_single_ancestor_load) {
+          Metasploit::Cache::Module::Ancestor::Load.new(
+              logger: logger,
+              maximum_version: 4,
+              module_ancestor: payload_single_ancestor
+          )
+        }
+
+        let(:payload_single_handled_class_load) {
+          Metasploit::Cache::Payload::Single::Handled::Class::Load.new(
+              handler_module: payload_single_unhandled_instance_load.metasploit_module_instance.handler_klass,
+              logger: logger,
+              metasploit_module: payload_single_ancestor_load.metasploit_module,
+              payload_single_handled_class: metasploit_cache_payload_single_handled_class,
+              payload_superclass: payload_superclass
+          )
+        }
+
+        let(:payload_single_unhandled_class) {
+          payload_single_unhandled_instance.payload_single_unhandled_class
+        }
+
+        let(:payload_single_unhandled_class_load) {
+          Metasploit::Cache::Payload::Unhandled::Class::Load.new(
+              logger: logger,
+              metasploit_module: payload_single_ancestor_load.metasploit_module,
+              payload_unhandled_class: payload_single_unhandled_class,
+              payload_superclass: payload_superclass
+          )
+        }
+
+        let(:payload_single_unhandled_instance) {
+          metasploit_cache_payload_single_handled_class.payload_single_unhandled_instance
+        }
+
+        let(:payload_single_unhandled_instance_load) {
+          Metasploit::Cache::Module::Instance::Load.new(
+              ephemeral_class: Metasploit::Cache::Payload::Single::Unhandled::Instance::Ephemeral,
+              logger: logger,
+              metasploit_framework: metasploit_framework,
+              metasploit_module_class: payload_single_unhandled_class_load.metasploit_class,
+              module_instance: payload_single_unhandled_instance
+          )
+        }
+
+        let(:payload_superclass) {
+          Metasploit::Cache::Direct::Class::Superclass
+        }
+
+        it 'is loadable' do
+          expect(payload_single_handled_class_load).to be_valid
+          expect(metasploit_cache_payload_single_handled_class).to be_persisted
+        end
+
+        context 'Metasploit::Cache::Payload::Single::Handled::Class#payload_single_unhandled_instance' do
+          it 'is loadable' do
+            expect(payload_single_unhandled_instance_load).to be_valid
+            expect(payload_single_unhandled_instance).to be_persisted
+          end
+
+          context 'Metasploit::Cache::Payload::Single::Unhandled::Instance#payload_single_unhandled_class' do
+            it 'is loadable' do
+              expect(payload_single_unhandled_class_load).to be_valid
+              expect(payload_single_unhandled_class).to be_persisted
+            end
+
+            context 'Metasploit::Cache::Payload::Single::Unhandled::Class#ancestor' do
+              it 'is loadable' do
+                expect(payload_single_ancestor_load).to be_valid
+                expect(payload_single_ancestor).to be_persisted
+              end
+            end
+          end
+        end
+      end
     end
   end
 
