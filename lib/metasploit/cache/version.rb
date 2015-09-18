e9f9512e61afd405d4381301dbdd2b25ffec517e
Luke Imhoff <luke_imhoff@rapid7.com>
2015-09-18 12:38:19 -0500

    Update version for incompatibility
    MSP-12459

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -9,9 +9,9 @@ module Metasploit
       # The major version number.
       MAJOR = 0
       # The minor version number, scoped to the {MAJOR} version number.
-      MINOR = 77
+      MINOR = 78
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 1
+      PATCH = 0
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
       PRERELEASE = 'load-by-reference-name-direct-class'
 
