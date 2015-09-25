67d1630bb0860945b00fff79f8146f27a65a5b8a
Luke Imhoff <luke_imhoff@rapid7.com>
2015-09-25 12:41:15 -0500

    Use Ephemeral::AttributeSet.existing_by_attribute_value
    MSP-12460
    
    Use for Author, EmailAddress, and License where it wasn't used before so
    there's less code to change for transaction isolation and retry.

--- lib/metasploit/cache/license/ephemeral.rb
+++ lib/metasploit/cache/license/ephemeral.rb
@@ -6,30 +6,15 @@ module Metasploit::Cache::License::Ephemeral
   # @param existing_abbreviation_set [Set<String>] Set of {Metasploit::Cache::License#abbreviation} to preload
   # @return [Hash{String => Metasploit::Cache::License}]
   def self.by_abbreviation(existing_abbreviation_set:)
-    existing_by_abbreviation(abbreviation_set: existing_abbreviation_set).tap { |hash|
+    Metasploit::Cache::Ephemeral::AttributeSet.existing_by_attribute_value(
+        attribute: :abbreviation,
+        scope: Metasploit::Cache::License,
+        value_set: existing_abbreviation_set
+    ).tap { |hash|
       hash.default_proc = Metasploit::Cache::Ephemeral.create_unique_proc(
           Metasploit::Cache::License,
           :abbreviation
       )
     }
-  end
-
-  # Maps {Metasploit::Cache::License#abbreviation} to existing {Metasploit::Cache::License}.
-  #
-  # @param abbreviation_set [Set<String>] Set of license abbreviations from added attributes set.
-  # @return [Hash{String => Metasploit::Cache::License}]
-  def self.existing_by_abbreviation(abbreviation_set:)
-    # avoid querying database with `IN (NULL)`
-    if abbreviation_set.empty?
-      {}
-    else
-      # get pre-existing licenses in bulk
-      Metasploit::Cache::License.where(
-          # AREL cannot visit Set
-          abbreviation: abbreviation_set.to_a
-      ).each_with_object({}) { |license, license_by_abbreviation|
-        license_by_abbreviation[license.abbreviation] = license
-      }
-    end
   end
 end
