534dea1a77103628a6a6a22a7683c62182fe0c01
Luke Imhoff <luke_imhoff@rapid7.com>
2015-09-25 13:32:30 -0500

    Repeatable read transaction when getting existing records
    MSP-12460
    
    Sqlite3 calls a step function, which can trigger a BusyExceptin, so
    wrap in a transaction to get retry.  Isolate in repeatable read since
    the where isn't writing.

--- lib/metasploit/cache/ephemeral/attribute_set.rb
+++ lib/metasploit/cache/ephemeral/attribute_set.rb
@@ -16,19 +16,23 @@ module Metasploit::Cache::Ephemeral::AttributeSet
   # Maps values of `attribute` in `value_set` to records in `scope`.
   #
   # @param attribute [Symbol] name of attribute with `value_set` values.
-  # @param scope [#where, #each_with_object] Scope for looking up persisted record that have `attribute_set` values on
-  #   `attribute`.
+  # @param scope [#isolation_level, #transaction, #where] Scope for looking up persisted record that have
+  #   `attribute_set` values on `attribute`.
   # @param value_set [Enumerable<Object>, #to_a] Set of values of `attribute` on `active_record_subclass`
   # @return [Hash{Object => ActiveRecord::Base}] Maps values of `attribute` to instances of `active_record_subclass`.
   def self.existing_by_attribute_value(attribute:, scope:, value_set:)
     if value_set.empty?
       {}
     else
-      scope.where(
-               # AREL cannot visit Set
-               attribute => value_set.to_a
-      ).each_with_object({}) { |record, record_by_attribute_value|
-        record_by_attribute_value[record.public_send(attribute)] = record
+      scope.isolation_level(:repeatable_read) {
+        scope.transaction {
+          scope.where(
+              # AREL cannot visit Set
+              attribute => value_set.to_a
+          ).each_with_object({}) { |record, record_by_attribute_value|
+            record_by_attribute_value[record.public_send(attribute)] = record
+          }
+        }
       }
     end
   end
