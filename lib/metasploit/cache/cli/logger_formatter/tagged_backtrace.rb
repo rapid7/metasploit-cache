0f66bac6dc15a0d240a09828ccaf05a66296d672
Luke Imhoff <luke_imhoff@rapid7.com>
2015-09-28 15:32:59 -0500

    Fix YARD docs
    MSP-12460

--- lib/metasploit/cache/cli/logger_formatter/tagged_backtrace.rb
+++ lib/metasploit/cache/cli/logger_formatter/tagged_backtrace.rb
@@ -14,7 +14,7 @@ module Metasploit::Cache::CLI::LoggerFormatter::TaggedBacktrace
   # Converts the message to a string using `Logger::Formatter#msg2str` so exceptions include their backtraces.
   #
   # @param severity [String] severity level
-  # @param data_time [DateTime] when this message occured
+  # @param date_time [DateTime] when this message occured
   # @param _progname [String] Ignored
   # @param message [String, Exception, #inspect] Logger message, exception that was raised, or object to inspect
   # @return [void]
@@ -28,10 +28,20 @@ module Metasploit::Cache::CLI::LoggerFormatter::TaggedBacktrace
 
   private
 
+  # Whether to accept the message for formating or return an empty string from {#call}.
+  #
+  # @param message [Object]
+  # @return [true] if `message` is not a `String` or `message` is a `String`, but does not begin with
+  #   {SQLITE#_BINARY_DATA_PREFIX}.
+  # @return [false] if `message` is a `String` and it begins with {SQLITE3_BINARY_DATA_PREFIX}
   def accept(message)
     !message.is_a?(String) || !message.start_with?(SQLITE3_BINARY_DATA_PREFIX)
   end
 
+  # Converts `message` to a `String`.
+  #
+  # @param message [Object]
+  # @return [String]
   def msg2str(message)
     case message
     when ::Exception
@@ -49,6 +59,10 @@ module Metasploit::Cache::CLI::LoggerFormatter::TaggedBacktrace
     end
   end
 
+  # Whetherto reject the line from the exception backtrace.
+  #
+  # @return [false] if `line` starts with `Dir.pwd`
+  # @return [true] otherwise
   def reject_backtrace_line(line)
     !line.start_with?(Dir.pwd)
   end
