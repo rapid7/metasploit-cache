23f19271554d41992318448554f1877c87b95ea5
Luke Imhoff <luke_imhoff@rapid7.com>
2015-09-28 15:11:23 -0500

    Filter log to get under $MB travis-ci limit
    MSP-12460
    
    Filter out 'Binary data inserted for `string` type on column' and
    backtrace lines that don't start with the PWD.

--- lib/metasploit/cache/cli/logger_formatter/tagged_backtrace.rb
+++ lib/metasploit/cache/cli/logger_formatter/tagged_backtrace.rb
@@ -1,6 +1,16 @@
 # Overrides `#call` from `ActiveSupport::TaggedLogging::Formatter` so that exceptions will print their backtraces like
 # `Logger::Formatter` from the standard library.
 module Metasploit::Cache::CLI::LoggerFormatter::TaggedBacktrace
+  #
+  # CONSTANTS
+  #
+
+  SQLITE3_BINARY_DATA_PREFIX = 'Binary data inserted for `string` type on column'
+
+  #
+  # Instance Methods
+  #
+
   # Converts the message to a string using `Logger::Formatter#msg2str` so exceptions include their backtraces.
   #
   # @param severity [String] severity level
@@ -9,6 +19,37 @@ module Metasploit::Cache::CLI::LoggerFormatter::TaggedBacktrace
   # @param message [String, Exception, #inspect] Logger message, exception that was raised, or object to inspect
   # @return [void]
   def call(severity, date_time, _progname, message)
-    "[#{format_datetime(date_time)}][#{severity}]#{tags_text} #{msg2str(message)}\n"
+    if accept(message)
+      "[#{format_datetime(date_time)}][#{severity}]#{tags_text} #{msg2str(message)}\n"
+    else
+      ''
+    end
+  end
+
+  private
+
+  def accept(message)
+    !message.is_a?(String) || !message.start_with?(SQLITE3_BINARY_DATA_PREFIX)
+  end
+
+  def msg2str(message)
+    case message
+    when ::Exception
+      lines = ["#{message.message} (#{message.class})"]
+
+      message.backtrace.each do |line|
+        unless reject_backtrace_line(line)
+          lines << line
+        end
+      end
+
+      lines.join("\n")
+    else
+      super
+    end
+  end
+
+  def reject_backtrace_line(line)
+    !line.start_with?(Dir.pwd)
   end
 end
