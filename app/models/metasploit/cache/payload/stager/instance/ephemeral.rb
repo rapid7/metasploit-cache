3d42d18e3930e9c99991fea03f1128190a2f6db4
Luke Imhoff <luke_imhoff@rapid7.com>
2015-10-08 14:01:12 -0500

    Use Ephemeral.persist in Payload::Stager::Instance::Ephemeral
    MSP-12461

--- app/models/metasploit/cache/payload/stager/instance/ephemeral.rb
+++ app/models/metasploit/cache/payload/stager/instance/ephemeral.rb
@@ -55,47 +55,36 @@ class Metasploit::Cache::Payload::Stager::Instance::Ephemeral < Metasploit::Mode
   #   Giving `to` saves a database lookup if {#payload_stager_instance} is not loaded.
   # @return [Metasploit::Cache:Payload::Stager::Instance] `#persisted?` will be `false` if saving fails.
   def persist(to: payload_stager_instance)
-    [:description, :name, :privileged].each do |attribute|
-      to.send("#{attribute}=", metasploit_module_instance.send(attribute))
-    end
+    persisted = nil
 
-    synchronizers = [
-        Metasploit::Cache::Architecturable::Ephemeral::ArchitecturableArchitectures,
-        Metasploit::Cache::Contributable::Ephemeral::Contributions,
-        Metasploit::Cache::Licensable::Ephemeral::LicensableLicenses,
-        Metasploit::Cache::Payload::Handable::Ephemeral::Handler,
-        Metasploit::Cache::Platformable::Ephemeral::PlatformablePlatforms
-    ]
+    ActiveRecord::Base.connection_pool.with_connection do
+      [:description, :name, :privileged].each do |attribute|
+        to.send("#{attribute}=", metasploit_module_instance.send(attribute))
+      end
 
-    synchronized = nil
+      synchronizers = [
+          Metasploit::Cache::Architecturable::Ephemeral::ArchitecturableArchitectures,
+          Metasploit::Cache::Contributable::Ephemeral::Contributions,
+          Metasploit::Cache::Licensable::Ephemeral::LicensableLicenses,
+          Metasploit::Cache::Payload::Handable::Ephemeral::Handler,
+          Metasploit::Cache::Platformable::Ephemeral::PlatformablePlatforms
+      ]
 
-    with_payload_stager_instance_tag(to) do |tagged|
-      synchronized = synchronizers.reduce(to) { |block_destination, synchronizer|
-        synchronizer.synchronize(
-            destination: block_destination,
-            logger: logger,
-            source: metasploit_module_instance
-        )
-      }
-
-      saved = ActiveRecord::Base.connection_pool.with_connection {
-        synchronized_class = synchronized.class
-
-        synchronized_class.isolation_level(:serializable) {
-          synchronized_class.transaction {
-            synchronized.batched_save
-          }
+      with_payload_stager_instance_tag(to) do |tagged|
+        synchronized = synchronizers.reduce(to) { |block_destination, synchronizer|
+          synchronizer.synchronize(
+              destination: block_destination,
+              logger: logger,
+              source: metasploit_module_instance
+          )
         }
-      }
 
-      unless saved
-        tagged.error {
-          "Could not be persisted to #{synchronized.class}: #{synchronized.errors.full_messages.to_sentence}"
-        }
+        persisted = Metasploit::Cache::Ephemeral.persist logger: tagged,
+                                                         record: synchronized
       end
     end
 
-    synchronized
+    persisted
   end
 
   private
@@ -117,9 +106,7 @@ class Metasploit::Cache::Payload::Stager::Instance::Ephemeral < Metasploit::Mode
   # @yieldreturn [void]
   # @return [void]
   def with_payload_stager_instance_tag(payload_stager_instance, &block)
-    real_path = ActiveRecord::Base.connection_pool.with_connection {
-      payload_stager_instance.payload_stager_class.ancestor.real_pathname.to_s
-    }
+    real_path = payload_stager_instance.payload_stager_class.ancestor.real_pathname.to_s
 
     Metasploit::Cache::Logged.with_tagged_logger(ActiveRecord::Base, logger, real_path, &block)
   end
