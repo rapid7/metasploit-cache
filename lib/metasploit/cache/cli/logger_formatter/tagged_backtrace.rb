62af0f0f341dc91790114c65b2f1384c11ba83a8
wchen-r7 <wei_chen@rapid7.com>
2015-11-09 15:48:50 -0600

    Fix a documentation typo in tagged_backtrace.rb
    data_time should be date_time

--- lib/metasploit/cache/cli/logger_formatter/tagged_backtrace.rb
+++ lib/metasploit/cache/cli/logger_formatter/tagged_backtrace.rb
@@ -4,7 +4,7 @@ module Metasploit::Cache::CLI::LoggerFormatter::TaggedBacktrace
   # Converts the message to a string using `Logger::Formatter#msg2str` so exceptions include their backtraces.
   #
   # @param severity [String] severity level
-  # @param data_time [DateTime] when this message occured
+  # @param date_time [DateTime] when this message occured
   # @param _progname [String] Ignored
   # @param message [String, Exception, #inspect] Logger message, exception that was raised, or object to inspect
   # @return [void]
