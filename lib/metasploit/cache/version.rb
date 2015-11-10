52d7e896bb4c9340f5cb897521c962a577a64071
Luke Imhoff <luke_imhoff@rapid7.com>
2015-11-09 22:03:07 -0600

    Remove PRERELEASE version

--- lib/metasploit/cache/version.rb
+++ lib/metasploit/cache/version.rb
@@ -12,8 +12,6 @@ module Metasploit
       MINOR = 79
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 0
-      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'load-by-reference-name-payload-single-class'
 
       #
       # Module Methods
